913db7912d3199d11a8e8f204d0d342dd0501b86
ntminer <2362120103@qq.com>
2018-12-28 14:13:12 +0800

    修改一个菜单图标

--- src/AppUI/Views/Ucs/AboutPage.xaml.cs
+++ src/AppUI/Views/Ucs/AboutPage.xaml.cs
@@ -4,13 +4,9 @@ using System.Windows.Controls;
 namespace NTMiner.Views.Ucs {
     public partial class AboutPage : UserControl {
         public static void ShowWindow(string appType) {
-            string iconImage = "/NTMinerWpf;component/Styles/Images/logo32.png";
-            if (appType == "ControlCenter") {
-                iconImage = "/NTMinerWpf;component/Styles/Images/cc32.png";
-            }
             ContainerWindow.ShowWindow(new ContainerWindowViewModel {
                 Title = "关于",
-                IconImage = iconImage,
+                IconName = "Icon_About",
                 CloseVisible = System.Windows.Visibility.Visible,
                 FooterVisible = System.Windows.Visibility.Collapsed
             }, ucFactory: (window) => new AboutPage(appType), fixedSize: true);
