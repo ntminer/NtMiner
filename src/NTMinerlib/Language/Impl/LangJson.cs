c58ce71f7dd4c2bbf5ec8e4411f2cca17ff3380d
ntminer <2362120103@qq.com>
2018-12-30 21:29:42 +0800

    ReadOnly

---
+++ src/NTMinerlib/Language/Impl/LangJson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NTMiner.Language.Impl {
+    public class LangJson {
+        public LangJson() {
+            this.Langs = new List<Lang>();
+            this.LangItems = new List<LangItem>();
+        }
+
+        public bool Exists<T>(Guid key) where T : IDbEntity<Guid> {
+            return GetAll<T>().Any(a => a.GetId() == key);
+        }
+
+        public T GetByKey<T>(Guid key) where T : IDbEntity<Guid> {
+            return GetAll<T>().FirstOrDefault(a => a.GetId() == key);
+        }
+
+        public IList<T> GetAll<T>() where T : IDbEntity<Guid> {
+            string typeName = typeof(T).Name;
+            switch (typeName) {
+                case nameof(Lang):
+                    return (IList<T>)this.Langs;
+                case nameof(LangItem):
+                    return (IList<T>)this.LangItems;
+                default:
+                    return new List<T>();
+            }
+        }
+
+        public ulong TimeStamp { get; set; }
+
+        public List<Lang> Langs { get; set; }
+        public List<LangItem> LangItems { get; set; }
+    }
+}
--- src/NTMinerlib/Language/Impl/ReadOnlyLanguageRepository`1.cs
+++ src/NTMinerlib/Language/Impl/ReadOnlyLanguageRepository`1.cs
@@ -1,25 +1,38 @@
 ﻿using NTMiner.Repositories;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 
 namespace NTMiner.Language.Impl {
     public class ReadOnlyLanguageRepository<T> : IRepository<T> where T : class, IDbEntity<Guid> {
+        private static readonly LangJson data;
+
+        static ReadOnlyLanguageRepository() {
+            string langJsonFileFullName = Path.Combine(Global.GlobalDirFullName, "lang.json");
+            if (!File.Exists(langJsonFileFullName)) {
+                data = new LangJson();
+            }
+            else {
+                data = Global.JsonSerializer.Deserialize<LangJson>(File.ReadAllText(langJsonFileFullName));
+            }
+        }
+
+        public ReadOnlyLanguageRepository() { }
+
         public void Add(T entity) {
             throw new NotImplementedException();
         }
 
         public bool Exists(Guid key) {
-            throw new NotImplementedException();
+            return data.Exists<T>(key);
         }
 
         public IList<T> GetAll() {
-            throw new NotImplementedException();
+            return data.GetAll<T>();
         }
 
         public T GetByKey(Guid key) {
-            throw new NotImplementedException();
+            return data.GetByKey<T>(key);
         }
 
         public void Remove(Guid id) {
