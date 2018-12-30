ebcc302da5685d5b455a704556b8f8af76b14b2a
ntminer <2362120103@qq.com>
2018-12-30 22:13:15 +0800

    语言

--- src/NTMinerWpf/Vms/LangItemViewModels.cs
+++ src/NTMinerWpf/Vms/LangItemViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using NTMiner.Language;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,10 +7,60 @@ namespace NTMiner.Vms {
     public class LangItemViewModels : ViewModelBase {
         public static readonly LangItemViewModels Current = new LangItemViewModels();
 
-        private readonly Dictionary<ILang, Dictionary<string, List<LangItemViewModel>>> _dicByLangAndView = new Dictionary<ILang, Dictionary<string, List<LangItemViewModel>>>();
+        private readonly Dictionary<Guid, LangItemViewModel> _dicById = new Dictionary<Guid, LangItemViewModel>();
+        private readonly Dictionary<LangViewModel, Dictionary<string, List<LangItemViewModel>>> _dicByLangAndView = new Dictionary<LangViewModel, Dictionary<string, List<LangItemViewModel>>>();
 
         private LangItemViewModels() {
-            foreach (var lang in LangSet.Instance) {
+            Global.Access<LangItemAddedEvent>(
+                Guid.Parse("60FD738F-F260-4EF2-A60A-66B04EC6B243"),
+                "添加了语言项后刷新VM内存",
+                LogEnum.None,
+                action: message => {
+                    LangViewModel langVm;
+                    if (!LangViewModels.Current.TryGetLangVm(message.Source.LangId, out langVm)) {
+                        if (!_dicByLangAndView.ContainsKey(langVm)) {
+                            var dic = new Dictionary<string, List<LangItemViewModel>>();
+                            _dicByLangAndView.Add(langVm, dic);
+                            if (!dic.ContainsKey(message.Source.ViewId)) {
+                                dic.Add(message.Source.ViewId, new List<LangItemViewModel>());
+                            }
+                            dic[message.Source.ViewId].Add(new LangItemViewModel(message.Source));
+                        }
+                    }
+                });
+            Global.Access<LangItemUpdatedEvent>(
+                Guid.Parse("A4DCFEE0-FA36-4D84-9D98-0D4455BE1EA7"),
+                "更新了语言项后刷新VM内存",
+                LogEnum.None,
+                action: message => {
+                    LangItemViewModel langItemVm;
+                    if (_dicById.TryGetValue(message.Source.GetId(), out langItemVm)) {
+                        langItemVm.Update(message.Source);
+                    }
+                });
+            Global.Access<LangItemRemovedEvent>(
+                Guid.Parse("4AA8EA72-7BD9-45D8-B798-EF505C665572"),
+                "删除了语言项后刷新VM内存",
+                LogEnum.None,
+                action: message => {
+                    LangItemViewModel langItemVm;
+                    if (_dicById.TryGetValue(message.Source.GetId(), out langItemVm)) {
+                        _dicById.Remove(langItemVm.Id);
+                        LangViewModel langVm;
+                        if (LangViewModels.Current.TryGetLangVm(langItemVm.LangId, out langVm)) {
+                            if (_dicByLangAndView.ContainsKey(langVm) && _dicByLangAndView[langVm].ContainsKey(langItemVm.ViewId)) {
+                                _dicByLangAndView[langVm][langItemVm.ViewId].Remove(langItemVm);
+                                if (_dicByLangAndView[langVm][langItemVm.ViewId].Count == 0) {
+                                    _dicByLangAndView[langVm].Remove(langItemVm.ViewId);
+                                    if (_dicByLangAndView[langVm].Count == 0) {
+                                        _dicByLangAndView.Remove(langVm);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            foreach (var lang in LangViewModels.Current.LangVms) {
                 var dic = new Dictionary<string, List<LangItemViewModel>>();
                 _dicByLangAndView.Add(lang, dic);
                 foreach (var item in LangItemSet.Instance.GetLangItems(lang)) {
@@ -20,7 +71,7 @@ namespace NTMiner.Vms {
             }
         }
 
-        public List<LangItemViewModel> GetLangItemVms(ILang lang, string viewId) {
+        public List<LangItemViewModel> GetLangItemVms(LangViewModel lang, string viewId) {
             if (!_dicByLangAndView.ContainsKey(lang)) {
                 return new List<LangItemViewModel>();
             }
--- src/NTMinerWpf/Vms/LangViewModels.cs
+++ src/NTMinerWpf/Vms/LangViewModels.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using NTMiner.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NTMiner.Vms {
     public class LangViewModels : ViewModelBase {
@@ -7,7 +10,36 @@ namespace NTMiner.Vms {
         private readonly List<LangViewModel> _langVms = new List<LangViewModel>();
 
         private LangViewModels() {
-            foreach (var lang in Language.LangSet.Instance) {
+            Global.Access<LangAddedEvent>(
+                Guid.Parse("F0417261-877F-4310-895F-2B14099B2ABF"),
+                "添加了语言后刷新VM内存",
+                LogEnum.None,
+                action: message => {
+                    _langVms.Add(new LangViewModel(message.Source));
+                    OnPropertyChanged(nameof(LangVms));
+                });
+            Global.Access<LangUpdatedEvent>(
+                Guid.Parse("646181E6-044D-4D2F-B357-A3B376DAE680"),
+                "修改了语言后刷新VM内存",
+                LogEnum.None,
+                action: message => {
+                    LangViewModel langVm = _langVms.FirstOrDefault(a => a.Id == message.Source.GetId());
+                    if (langVm != null) {
+                        langVm.Update(message.Source);
+                    }
+                });
+            Global.Access<LangRemovedEvent>(
+                Guid.Parse("3684144A-26C8-47B9-881D-E66930A6E87D"),
+                "删除了语言后刷新VM内存",
+                LogEnum.None,
+                action: message => {
+                    LangViewModel langVm = _langVms.FirstOrDefault(a => a.Id == message.Source.GetId());
+                    if (langVm != null) {
+                        _langVms.Remove(langVm);
+                        OnPropertyChanged(nameof(LangVms));
+                    }
+                });
+            foreach (var lang in LangSet.Instance) {
                 _langVms.Add(new LangViewModel(lang));
             }
         }
@@ -16,6 +48,11 @@ namespace NTMiner.Vms {
             get {
                 return _langVms;
             }
+        }
+
+        public bool TryGetLangVm(Guid langId, out LangViewModel lang) {
+            lang = _langVms.FirstOrDefault(a => a.Id == langId);
+            return lang != null;
         }
     }
 }
