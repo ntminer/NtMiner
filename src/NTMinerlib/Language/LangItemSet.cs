9e12be8e327118258f33daa803f4a9fc4406f2a7
ntminer <2362120103@qq.com>
2018-12-31 20:04:37 +0800

    Lang CRUD

--- src/NTMinerlib/Language/LangItemSet.cs
+++ src/NTMinerlib/Language/LangItemSet.cs
@@ -9,8 +9,73 @@ namespace NTMiner.Language {
         public static readonly LangItemSet Instance = new LangItemSet();
 
         private readonly Dictionary<ILang, Dictionary<string, List<ILangItem>>> _dicByLangAndView = new Dictionary<ILang, Dictionary<string, List<ILangItem>>>();
+        private readonly Dictionary<Guid, LangItem> _dicById = new Dictionary<Guid, LangItem>();
 
-        private LangItemSet() { }
+        private LangItemSet() {
+            Global.Access<AddLangItemCommand>(
+                Guid.Parse("07AC4BE6-AB09-48D2-A3D7-8653EE52CC43"),
+                "处理添加语言项命令",
+                LogEnum.None,
+                action: message=> {
+                    if (_dicById.ContainsKey(message.Input.GetId())) {
+                        return;
+                    }
+                    ILang lang;
+                    if (LangSet.Instance.TryGetLang(message.Input.LangId, out lang)) {
+                        if (!_dicByLangAndView.ContainsKey(lang)) {
+                            _dicByLangAndView.Add(lang, new Dictionary<string, List<ILangItem>>());
+                        }
+                        var dic = _dicByLangAndView[lang];
+                        if (!dic.ContainsKey(message.Input.ViewId)) {
+                            dic.Add(message.Input.ViewId, new List<ILangItem>());
+                        }
+                        var entity = new LangItem().Update(message.Input);
+                        dic[message.Input.ViewId].Add(entity);
+                        _dicById.Add(message.Input.GetId(), entity);
+                        var repository = Repository.CreateLanguageRepository<LangItem>();
+                        repository.Add(entity);
+                    }
+                });
+            Global.Access<UpdateLangItemCommand>(
+                Guid.Parse("CEC2EFC5-4F92-4226-ADCE-BE36D8968B9E"),
+                "处理修改语言项命令",
+                LogEnum.None,
+                action: message => {
+                    if (_dicById.ContainsKey(message.Input.GetId())) {
+                        var entity = _dicById[message.Input.GetId()];
+                        entity.Update(message.Input);
+                        var repository = Repository.CreateLanguageRepository<LangItem>();
+                        repository.Update(entity);
+                    }
+                });
+            Global.Access<RemoveLangItemCommand>(
+                Guid.Parse("3827E59B-872D-45E6-8512-7EC22E1BE6E3"),
+                "处理删除语言项命令",
+                LogEnum.None,
+                action: message => {
+                    if (_dicById.ContainsKey(message.EntityId)) {
+                        var entity = _dicById[message.EntityId];
+                        _dicById.Remove(message.EntityId);
+                        ILang lang;
+                        if (LangSet.Instance.TryGetLang(entity.LangId, out lang)) {
+                            if (_dicByLangAndView.ContainsKey(lang)) {
+                                var dic = _dicByLangAndView[lang];
+                                if (dic.ContainsKey(entity.ViewId) && dic[entity.ViewId].Contains(entity)) {
+                                    dic[entity.ViewId].Remove(entity);
+                                    if (dic[entity.ViewId].Count == 0) {
+                                        dic.Remove(entity.ViewId);
+                                    }
+                                }
+                                if (_dicByLangAndView.Count == 0) {
+                                    _dicByLangAndView.Remove(lang);
+                                }
+                            }
+                        }
+                        var repository = Repository.CreateLanguageRepository<LangItem>();
+                        repository.Remove(entity.Id);
+                    }
+                });
+        }
 
         private bool _isInited = false;
         private object _locker = new object();
@@ -100,7 +165,7 @@ namespace NTMiner.Language {
             if (!dic.ContainsKey(viewId)) {
                 return new List<ILangItem>();
             }
-            return dic[viewId];
+            return dic[viewId].Cast<ILangItem>().ToList();
         }
 
         public Dictionary<string, List<ILangItem>> GetLangItems(ILang lang) {
--- src/NTMinerlib/Language/LangSet.cs
+++ src/NTMinerlib/Language/LangSet.cs
@@ -9,7 +9,7 @@ namespace NTMiner.Language {
     public class LangSet : IEnumerable<ILang> {
         public static readonly LangSet Instance = new LangSet();
 
-        private readonly List<ILang> _langs = new List<ILang>() {
+        private readonly List<Lang> _langs = new List<Lang>() {
             new Lang {
                 Id = new Guid("584398F7-5BDD-41B3-94C6-50A14F23DB71"),
                 Code = "en",
@@ -22,7 +22,53 @@ namespace NTMiner.Language {
             }
         };
 
-        private LangSet() { }
+        private LangSet() {
+            Global.Access<AddLangCommand>(
+                Guid.Parse("75818F82-D124-48B0-8138-D150D77EC557"),
+                "处理添加语言命令",
+                LogEnum.None,
+                action: message => {
+                    if (_langs.All(a => a.GetId() != message.Input.GetId() && a.Code != message.Input.Code)) {
+                        Lang entity = new Lang().Update(message.Input);
+                        _langs.Add(entity);
+                        var repository = Repository.CreateLanguageRepository<Lang>();
+                        repository.Add(entity);
+                    }
+                });
+            Global.Access<UpdateLangCommand>(
+                Guid.Parse("C6D2436E-C255-433F-8FAF-4E1D00570BF1"),
+                "处理修改语言命令",
+                LogEnum.None,
+                action: message => {
+                    Lang entity = _langs.FirstOrDefault(a => a.GetId() == message.Input.GetId());
+                    if (entity != null) {
+                        entity.Update(message.Input);
+                        var repository = Repository.CreateLanguageRepository<Lang>();
+                        repository.Update(entity);
+                    }
+                });
+            Global.Access<RemoveLangCommand>(
+                Guid.Parse("8C421769-23EB-4FF0-A634-A5C2DC58CD92"),
+                "处理删除语言命令",
+                LogEnum.None,
+                action: message => {
+                    var entity = _langs.FirstOrDefault(a => a.GetId() == message.EntityId);
+                    if (entity != null) {
+                        var toRemoveLangItemIds = new List<Guid>();
+                        foreach (var g in LangItemSet.Instance.GetLangItems(entity)) {
+                            foreach (var langItem in g.Value) {
+                                toRemoveLangItemIds.Add(langItem.GetId());
+                            }
+                        }
+                        foreach (var id in toRemoveLangItemIds) {
+                            Global.Execute(new RemoveLangItemCommand(id));
+                        }
+                        _langs.Remove(entity);
+                        var repository = Repository.CreateLanguageRepository<Lang>();
+                        repository.Remove(entity.GetId());
+                    }
+                });
+        }
 
         private bool _isInited = false;
         private object _locker = new object();
@@ -59,6 +105,16 @@ namespace NTMiner.Language {
                 result = _langs.First();
             }
             return result;
+        }
+
+        public bool TryGetLang(string langCode, out ILang lang) {
+            lang = _langs.FirstOrDefault(a => a.Code == langCode);
+            return lang != null;
+        }
+
+        public bool TryGetLang(Guid langId, out ILang lang) {
+            lang = _langs.FirstOrDefault(a => a.GetId() == langId);
+            return lang != null;
         }
 
         public IEnumerator<ILang> GetEnumerator() {
