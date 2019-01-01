fe33af4cf2f58361f357fc64064fcddb4f1adf0a
ntminer <2362120103@qq.com>
2019-01-01 16:35:35 +0800

    界面

---
+++ src/NTMinerWpf/Views/ViewLang.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MahApps.Metro.Controls;
+using NTMiner.Vms;
+using System.Windows.Controls;
+
+namespace NTMiner.Views {
+    public partial class ViewLang : MetroWindow {
+        public static void ShowWindow(ViewLangViewModel vm) {
+            ViewLang window = new ViewLang(vm);
+            window.ShowDialog();
+        }
+
+        public ViewLangViewModel Vm {
+            get {
+                return (ViewLangViewModel)this.DataContext;
+            }
+        }
+
+        private ViewLang(ViewLangViewModel vm) {
+            this.DataContext = vm;
+            InitializeComponent();
+        }
+
+        private void MetroWindow_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed) {
+                this.DragMove();
+            }
+        }
+
+        private void CbLanguage_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
+            LangViewModel selectedItem = (LangViewModel)e.AddedItems[0];
+            if (selectedItem != Global.CurrentLang) {
+                Global.CurrentLang = selectedItem;
+            }
+            Vm.OnPropertyChanged(nameof(Vm.LangViewItemVms));
+        }
+
+        private void LangViewItemDataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            DataGrid dg = (DataGrid)sender;
+            if (dg.SelectedItem != null) {
+                LangViewItemViewModel poolVm = (LangViewItemViewModel)dg.SelectedItem;
+                poolVm.Edit.Execute(null);
+            }
+        }
+    }
+}
---
+++ src/NTMinerWpf/Vms/ViewLangViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace NTMiner.Vms {
+    public class ViewLangViewModel : ViewModelBase {
+        private LangViewModel _selectedLangVm;
+
+        public ViewLangViewModel(string viewId) {
+            this.ViewId = viewId;
+        }
+
+        public string ViewId { get; private set; }
+
+        public LangViewModels LangVms {
+            get {
+                return LangViewModels.Current;
+            }
+        }
+
+        public LangViewModel SelectedLanguage {
+            get => _selectedLangVm;
+            set {
+                _selectedLangVm = value;
+                OnPropertyChanged(nameof(SelectedLanguage));
+            }
+        }
+
+        public List<LangViewItemViewModel> LangViewItemVms {
+            get {
+                return LangViewItemViewModels.Current.GetLangItemVms(SelectedLanguage, ViewId);
+            }
+        }
+    }
+}
