cfdf32979dd14c0568433f8f4f5d334c8a10800f
ntminer <2362120103@qq.com>
2019-01-01 21:38:09 +0800

    语言

--- src/NTMinerWpf/ResourceDictionarySet.cs
+++ src/NTMinerWpf/ResourceDictionarySet.cs
@@ -1,4 +1,5 @@
 ﻿using NTMiner.Language;
+using System;
 using System.Collections.Generic;
 using System.Windows;
 
@@ -7,6 +8,21 @@ namespace NTMiner {
         public static readonly ResourceDictionarySet Instance = new ResourceDictionarySet();
 
         private readonly Dictionary<string, ResourceDictionary> _dicByViewId = new Dictionary<string, ResourceDictionary>();
+
+        private ResourceDictionarySet() {
+            Global.Access<LangViewItemUpdatedEvent>(
+                Guid.Parse("E537F225-11C6-4B0D-B2B3-BC111E4EFFEA"),
+                "更新了语言视图项后刷新WPF动态资源集",
+                LogEnum.None,
+                action: message => {
+                    ResourceDictionary resourceDictionary;
+                    if (TryGetResourceDic(message.Source.ViewId, out resourceDictionary)) {
+                        if (resourceDictionary.Contains(message.Source.Key) && Global.CurrentLang.GetId() == message.Source.LangId) {
+                            resourceDictionary[message.Source.Key] = message.Source.Value;
+                        }
+                    }
+                });
+        }
 
         public bool TryGetResourceDic(string viewId, out ResourceDictionary resourceDictionary) {
             resourceDictionary = null;
