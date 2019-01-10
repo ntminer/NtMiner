c62d5c0348ec91cad903aff6e7d437e28bec1ce3
ntminer <2362120103@qq.com>
2019-01-11 01:30:43 +0800

    窗口最大化按钮

--- src/NTMinerWpf/Controls/KbSystemMaxButton.cs
+++ src/NTMinerWpf/Controls/KbSystemMaxButton.cs
@@ -1,17 +1,21 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace NTMiner.Controls {
     public class KbSystemMaxButton : KbSystemButton {
         private Window targetWindow;
         public KbSystemMaxButton() {
+            this.Icon = (StreamGeometry)Application.Current.Resources["Icon_Max"];
             Click += delegate {
                 if (targetWindow == null) {
                     targetWindow = Window.GetWindow(this);
                 }
                 if (targetWindow.WindowState == WindowState.Normal) {
                     targetWindow.WindowState = WindowState.Maximized;
+                    this.Icon = (StreamGeometry)Application.Current.Resources["Icon_Maxed"];
                 } else if (targetWindow.WindowState == WindowState.Maximized) {
                     targetWindow.WindowState = WindowState.Normal;
+                    this.Icon = (StreamGeometry)Application.Current.Resources["Icon_Max"];
                 }
             };
         }
