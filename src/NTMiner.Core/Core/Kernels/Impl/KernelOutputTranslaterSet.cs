734bdc952ed7bdf33d730e0cd4a9d5580c4066f6
ntminer <2362120103@qq.com>
2019-01-12 14:32:59 +0800

    翻译

--- src/NTMiner.Core/Core/Kernels/Impl/KernelOutputTranslaterSet.cs
+++ src/NTMiner.Core/Core/Kernels/Impl/KernelOutputTranslaterSet.cs
@@ -256,7 +256,7 @@ namespace NTMiner.Core.Kernels.Impl {
                     }
                     Match match = regex.Match(input);
                     if (match.Success) {
-                        if (Global.Lang.Code == "cn-zh" || (isPre && consoleTranslater.IsPre)) {
+                        if (Global.Lang.Code == "zh-CN" || (isPre && consoleTranslater.IsPre)) {
                             if (!string.IsNullOrEmpty(consoleTranslater.Replacement)) {
                                 input = regex.Replace(input, consoleTranslater.Replacement);
                             }
