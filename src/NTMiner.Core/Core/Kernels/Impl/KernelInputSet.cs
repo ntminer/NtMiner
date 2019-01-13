c68f223240d0d1d96f4b872302b31431d644d4ff
ntminer <2362120103@qq.com>
2019-01-13 18:38:49 +0800

    内存数据集管理

--- src/NTMiner.Core/Core/Kernels/Impl/KernelInputSet.cs
+++ src/NTMiner.Core/Core/Kernels/Impl/KernelInputSet.cs
@@ -10,6 +10,67 @@ namespace NTMiner.Core.Kernels.Impl {
 
         public KernelInputSet(INTMinerRoot root) {
             _root = root;
+            Global.Access<AddKernelInputCommand>(
+                Guid.Parse("62D0B345-26F8-42BA-B7CD-E547C2B298C9"),
+                "添加内核输入组",
+                LogEnum.Log,
+                action: (message) => {
+                    InitOnece();
+                    if (message == null || message.Input == null || message.Input.GetId() == Guid.Empty) {
+                        throw new ArgumentNullException();
+                    }
+                    if (_dicById.ContainsKey(message.Input.GetId())) {
+                        return;
+                    }
+                    KernelInputData entity = new KernelInputData().Update(message.Input);
+                    _dicById.Add(entity.Id, entity);
+                    var repository = NTMinerRoot.CreateServerRepository<KernelInputData>();
+                    repository.Add(entity);
+
+                    Global.Happened(new KernelInputAddedEvent(entity));
+                });
+            Global.Access<UpdateKernelInputCommand>(
+                Guid.Parse("FED12C08-7BD7-4A8E-BD0B-A19075F4E8C4"),
+                "更新内核输入组",
+                LogEnum.Log,
+                action: (message) => {
+                    InitOnece();
+                    if (message == null || message.Input == null || message.Input.GetId() == Guid.Empty) {
+                        throw new ArgumentNullException();
+                    }
+                    if (string.IsNullOrEmpty(message.Input.Name)) {
+                        throw new ValidationException("KernelInput name can't be null or empty");
+                    }
+                    if (!_dicById.ContainsKey(message.Input.GetId())) {
+                        return;
+                    }
+                    KernelInputData entity = _dicById[message.Input.GetId()];
+                    entity.Update(message.Input);
+                    var repository = NTMinerRoot.CreateServerRepository<KernelInputData>();
+                    repository.Update(entity);
+
+                    Global.Happened(new KernelInputUpdatedEvent(entity));
+                });
+            Global.Access<RemoveKernelInputCommand>(
+                Guid.Parse("2227F6B9-5A2A-42AB-8147-05E245E2872F"),
+                "移除内核输入组",
+                LogEnum.Log,
+                action: (message) => {
+                    InitOnece();
+                    if (message == null || message.EntityId == Guid.Empty) {
+                        throw new ArgumentNullException();
+                    }
+                    if (!_dicById.ContainsKey(message.EntityId)) {
+                        return;
+                    }
+                    KernelInputData entity = _dicById[message.EntityId];
+                    _dicById.Remove(entity.GetId());
+                    var repository = NTMinerRoot.CreateServerRepository<KernelInputData>();
+                    repository.Remove(message.EntityId);
+
+                    Global.Happened(new KernelInputRemovedEvent(entity));
+                });
+            Global.Logger.InfoDebugLine(this.GetType().FullName + "接入总线");
         }
 
         private bool _isInited = false;
