de30d3dde9cbb9cff5b86fe5536aad90aefa67e1
ntminer <2362120103@qq.com>
2019-01-19 18:04:35 +0800

    优化

---
+++ src/NTMinerDaemon/ClientId.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+namespace NTMiner {
+    public static class ClientId {
+        public static readonly string AppFileFullName = Process.GetCurrentProcess().MainModule.FileName;
+        public const string NTMinerRegistrySubKey = @".DEFAULT\Software\NTMiner";
+    }
+}
