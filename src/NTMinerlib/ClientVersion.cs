e9ef31dad51045241435f4d39ec04b354d123de7
ntminer <2362120103@qq.com>
2019-01-20 17:28:18 +0800

    1.1.2

--- src/NTMinerlib/ClientVersion.cs
+++ src/NTMinerlib/ClientVersion.cs
@@ -1,6 +1,6 @@
 ﻿namespace NTMiner {
     public static class ClientVersion {
-        public const string VERSION = "1.1.1";
+        public const string VERSION = "1.1.2";
         public const string Tag = "谜麟";
         public static readonly string ServerJsonFileName = $"server{VERSION}.json";
         public static readonly string ServerLangJsonFileName = $"serverLang{VERSION}.json";
