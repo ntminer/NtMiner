9f3aae0a387ea692e7f912189ac2f2feaee114ec
ntminer <2362120103@qq.com>
2019-01-27 20:03:56 +0800

    test

---
+++ src/NTMinerServer/Services/AppSettingService.cs
@@ -0,0 +1,31 @@
+﻿using NTMiner.ServiceContracts;
+using NTMiner.ServiceContracts.DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTMiner.Services {
+    public class AppSettingService : IAppSettingService {
+        public GetAppSettingResponse GetAppSetting(string key) {
+            throw new NotImplementedException();
+        }
+
+        public GetAppSettingsResponse GetAppSettings(string[] keys) {
+            throw new NotImplementedException();
+        }
+
+        public GetAppSettingsResponse GetAppSettings() {
+            throw new NotImplementedException();
+        }
+
+        public ResponseBase SetAppSetting(SetAppSettingRequest request) {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() {
+            // nothing need to do
+        }
+    }
+}
--- src/NTMinerlib/ServiceContracts/IAppSettingService.cs
+++ src/NTMinerlib/ServiceContracts/IAppSettingService.cs
@@ -5,6 +5,7 @@ namespace NTMiner.ServiceContracts {
     public interface IAppSettingService : IDisposable {
         GetAppSettingsResponse GetAppSettings();
         GetAppSettingResponse GetAppSetting(string key);
+        GetAppSettingsResponse GetAppSettings(string[] keys);
         ResponseBase SetAppSetting(SetAppSettingRequest request);
     }
 }
