ce66a91850a1832b64997292b093c2ff5ed2acef
ntminer <2362120103@qq.com>
2019-01-31 21:14:03 +0800

    View

---
+++ src/AppUI/Vms/UserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using NTMiner.User;
+
+namespace NTMiner.Vms {
+    public class UserViewModel : ViewModelBase, IUser {
+        private string _loginName;
+        private string _password;
+        private string _description;
+
+        public string LoginName {
+            get => _loginName;
+            set {
+                _loginName = value;
+                OnPropertyChanged(nameof(LoginName));
+            }
+        }
+
+        public string Password {
+            get => _password;
+            set {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public string Description {
+            get => _description;
+            set {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+    }
+}
---
+++ src/AppUI/Vms/UserViewModels.cs
@@ -0,0 +1,43 @@
+﻿using NTMiner.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NTMiner.Vms {
+    public class UserViewModels : ViewModelBase {
+        public static readonly UserViewModels Current = new UserViewModels();
+
+        private readonly Dictionary<string, UserViewModel> _dicByLoginName = new Dictionary<string, UserViewModel>();
+
+        private UserViewModels() {
+            Global.Access<UserAddedEvent>(
+                Guid.Parse("866DD6F9-2A66-4578-90C0-E55AA7791E2C"),
+                "添加了用户后",
+                LogEnum.Console,
+                action: message => {
+
+                });
+            Global.Access<UserUpdatedEvent>(
+                Guid.Parse("D03BD781-ABC3-415B-86E5-20AA8FF34862"),
+                "更新了用户后",
+                LogEnum.Console,
+                action: message => {
+
+                });
+            Global.Access<UserAddedEvent>(
+                Guid.Parse("7F039C84-A429-4110-97FB-9A740C0238EA"),
+                "移除了用户后",
+                LogEnum.Console,
+                action: message => {
+
+                });
+        }
+
+        public List<UserViewModel> List {
+            get {
+                return _dicByLoginName.Values.ToList();
+            }
+        }
+    }
+}
