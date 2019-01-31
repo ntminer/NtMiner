ade0a5bc0f064a21529eaf9869980b52e1938768
ntminer <2362120103@qq.com>
2019-01-31 22:22:26 +0800

    User

--- src/AppUI/Vms/UserViewModel.cs
+++ src/AppUI/Vms/UserViewModel.cs
@@ -1,10 +1,61 @@
 ﻿using NTMiner.User;
+using NTMiner.Views;
+using NTMiner.Views.Ucs;
+using System;
+using System.Windows.Input;
 
 namespace NTMiner.Vms {
     public class UserViewModel : ViewModelBase, IUser {
         private string _loginName;
         private string _password;
         private string _description;
+
+        public ICommand Remove { get; private set; }
+        public ICommand Edit { get; private set; }
+        public ICommand Save { get; private set; }
+
+        public Action CloseWindow { get; set; }
+
+        public UserViewModel() {
+            if (!Design.IsInDesignMode) {
+                throw new InvalidProgramException();
+            }
+        }
+
+        public UserViewModel(IUser data) : this(data.LoginName) {
+            _password = data.Password;
+            _description = data.Description;
+        }
+
+        public UserViewModel(string loginName) {
+            _loginName = loginName;
+            this.Save = new DelegateCommand(() => {
+                if (string.IsNullOrEmpty(this.LoginName)) {
+                    return;
+                }
+                if (NTMinerRoot.Current.UserSet.Contains(this.LoginName)) {
+                    Global.Execute(new UpdateUserCommand(this));
+                }
+                else {
+                    Global.Execute(new AddUserCommand(this));
+                }
+                CloseWindow?.Invoke();
+            });
+            this.Edit = new DelegateCommand(() => {
+                if (string.IsNullOrEmpty(this.LoginName)) {
+                    return;
+                }
+                UserEdit.ShowEditWindow(this);
+            });
+            this.Remove = new DelegateCommand(() => {
+                if (string.IsNullOrEmpty(this.LoginName)) {
+                    return;
+                }
+                DialogWindow.ShowDialog(message: $"您确定删除{this.LoginName}矿池吗？", title: "确认", onYes: () => {
+                    Global.Execute(new RemoveUserCommand(this.LoginName));
+                }, icon: "Icon_Confirm");
+            });
+        }
 
         public string LoginName {
             get => _loginName;
