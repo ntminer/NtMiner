7c485c614645870143c03577f9ca8ee6824c5d05
ntminer <2362120103@qq.com>
2019-02-06 16:26:14 +0800

    test

--- src/NTMinerRpc/Properties/AssemblyInfo.cs
+++ src/NTMinerRpc/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("NTMinerRpc")]
+[assembly: AssemblyTitle("NTMinerRpc.Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NTMinerRpc")]
+[assembly: AssemblyProduct("NTMinerRpc.Server")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
