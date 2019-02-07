038860519327c533e9176a8f1752ac7423b3d631
ntminer <2362120103@qq.com>
2019-02-07 09:01:32 +0800

    test

--- src/NTMinerWebApi/Properties/AssemblyInfo.cs
+++ src/NTMinerWebApi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("NTMinerWebApi")]
+[assembly: AssemblyTitle("NTMinerServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NTMinerWebApi")]
+[assembly: AssemblyProduct("NTMinerServer")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
