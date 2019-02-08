3761e82733aa19e1f202d0682200d429ddbe2014
ntminer <2362120103@qq.com>
2019-02-08 17:09:15 +0800

    retry

--- src/ClientApp/App.xaml.cs
+++ src/ClientApp/App.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Interop;
@@ -31,13 +32,46 @@ namespace NTMiner {
         protected override void OnStartup(StartupEventArgs e) {
             RenderOptions.ProcessRenderMode = RenderMode.SoftwareOnly;
             if (!string.IsNullOrEmpty(CommandLineArgs.Upgrade)) {
-                Vms.AppStatic.Upgrade(CommandLineArgs.Upgrade, ()=> {
+                Vms.AppStatic.Upgrade(CommandLineArgs.Upgrade, () => {
                     Environment.Exit(0);
                 });
             }
             else {
                 try {
                     appMutex = new Mutex(true, _appPipName, out createdNew);
+                    if (!createdNew) {
+                        try {
+                            string location = NTMinerRegistry.GetLocation();
+                            if (!string.IsNullOrEmpty(location)) {
+                                bool retry = false;
+                                string processName = Path.GetFileNameWithoutExtension(location);
+                                Process[] processes = Process.GetProcessesByName(processName);
+                                Process currentProcess = Process.GetCurrentProcess();
+                                if (processes.Any(a => a.Id == currentProcess.Id) && (processes.Length == 1)) {
+                                    retry = true;
+                                }
+                                else if (processes.Length == 0) {
+                                    retry = true;
+                                }
+                                if (retry) {
+                                    int times = 0;
+                                    while (!createdNew && times < 10) {
+                                        try {
+                                            appMutex = new Mutex(true, _appPipName, out createdNew);
+                                            times++;
+                                            if (!createdNew) {
+                                                Thread.Sleep(200);
+                                            }
+                                        }
+                                        catch {
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        catch {
+                        }
+                    }
                 }
                 catch (Exception) {
                     createdNew = false;
