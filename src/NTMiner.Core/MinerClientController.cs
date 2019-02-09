1f8614d2007ef58a4cf73a222d4a3f48d4eab01f
ntminer <2362120103@qq.com>
2019-02-09 17:33:20 +0800

    test

--- src/NTMiner.Core/MinerClientController.cs
+++ src/NTMiner.Core/MinerClientController.cs
@@ -1,13 +1,41 @@
 ﻿using NTMiner.MinerClient;
+using NTMiner.User;
 using System;
 using System.Web.Http;
 
 namespace NTMiner.Controllers {
     public class MinerClientController : ApiController {
         [HttpPost]
+        public ResponseBase AddUser([FromBody]AddUserRequest request) {
+            if (request == null || string.IsNullOrEmpty(request.LoginName)) {
+                return ResponseBase.InvalidInput(Guid.Empty, "参数错误");
+            }
+            try {
+                if (NTMinerRoot.Current.UserSet.Contains(request.LoginName)) {
+                    return ResponseBase.ClientError(request.MessageId, $"登录名{request.LoginName}已经存在");
+                }
+                if (string.IsNullOrEmpty(request.Password)) {
+                    return ResponseBase.ClientError(request.MessageId, $"密码不能为空");
+                }
+                VirtualRoot.Execute(new AddUserCommand(request));
+                return ResponseBase.Ok(request.MessageId);
+            }
+            catch (Exception e) {
+                Logger.ErrorDebugLine(e.Message, e);
+                return ResponseBase.ServerError(request.MessageId, e.Message);
+            }
+        }
+
+        [HttpPost]
         public bool ShowMainWindow() {
-            VirtualRoot.Execute(new ShowMainWindowCommand());
-            return true;
+            try {
+                VirtualRoot.Execute(new ShowMainWindowCommand());
+                return true;
+            }
+            catch (Exception e) {
+                Logger.ErrorDebugLine(e.Message, e);
+                return false;
+            }
         }
 
         [HttpPost]
---
+++ src/NTMinerDataObjects/MinerClient/AddUserRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace NTMiner.MinerClient {
+    public class AddUserRequest : RequestBase, IUser {
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+    }
+}
