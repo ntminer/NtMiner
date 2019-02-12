f7f614ea8371e70fa9c9cd9bac84e5653a5d8c32
ntminer <2362120103@qq.com>
2019-02-12 21:35:12 +0800

    test

--- src/AppUI/Vms/ControlCenterWindowViewModel.cs
+++ src/AppUI/Vms/ControlCenterWindowViewModel.cs
@@ -6,26 +6,12 @@ namespace NTMiner.Vms {
     public class ControlCenterWindowViewModel : ViewModelBase {
         public static readonly ControlCenterWindowViewModel Current = new ControlCenterWindowViewModel();
 
-        private string _title = "开源矿工中控客户端";
-
         public ICommand ConfigMinerServerHost { get; private set; }
 
         private ControlCenterWindowViewModel() {
             this.ConfigMinerServerHost = new DelegateCommand(() => {
                 MinerServerHostConfig.ShowWindow();
             });
-        }
-
-        public string Title {
-            get {
-                return _title;
-            }
-            set {
-                if (_title != value) {
-                    _title = value;
-                    OnPropertyChanged(nameof(Title));
-                }
-            }
         }
 
         public LangViewModels LangVms {
