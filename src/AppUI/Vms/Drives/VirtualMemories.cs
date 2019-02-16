31f0cbe15f3bba0fd22563d120f9a30ae4dd4490
ntminer <2362120103@qq.com>
2019-02-16 16:34:05 +0800

    test

---
+++ src/AppUI/Vms/Drives/VirtualMemories.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NTMiner.Vms {
+    public class VirtualMemories : IEnumerable<VirtualMemory> {
+        public static readonly VirtualMemories Instance = new VirtualMemories();
+
+        private readonly Dictionary<string, VirtualMemory> _dic = new Dictionary<string, VirtualMemory>(StringComparer.OrdinalIgnoreCase);
+
+        private VirtualMemories() { }
+
+        public void Clear() {
+            _dic.Clear();
+        }
+
+        public void Add(string driveName, VirtualMemory item) {
+            if (!_dic.ContainsKey(driveName)) {
+                _dic.Add(driveName, item);
+            }
+        }
+
+        public VirtualMemory this[string driveName] {
+            get {
+                return _dic[driveName];
+            }
+        }
+
+        public bool Contains(string driveName) {
+            return _dic.ContainsKey(driveName);
+        }
+
+        public IEnumerator<VirtualMemory> GetEnumerator() {
+            return _dic.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return _dic.Values.GetEnumerator();
+        }
+    }
+}
--- src/AppUI/Vms/Drives/VirtualMemory.cs
+++ src/AppUI/Vms/Drives/VirtualMemory.cs
@@ -1,45 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace NTMiner.Vms {
-    public class VirtualMemories : IEnumerable<VirtualMemory> {
-        public static readonly VirtualMemories Instance = new VirtualMemories();
-
-        private readonly Dictionary<string, VirtualMemory> _dic = new Dictionary<string, VirtualMemory>(StringComparer.OrdinalIgnoreCase);
-
-        private VirtualMemories() { }
-
-        public void Clear() {
-            _dic.Clear();
-        }
-
-        public void Add(string driveName, VirtualMemory item) {
-            if (!_dic.ContainsKey(driveName)) {
-                _dic.Add(driveName, item);
-            }
-        }
-
-        public VirtualMemory this[string driveName] {
-            get {
-                return _dic[driveName];
-            }
-        }
-
-        public bool Contains(string driveName) {
-            return _dic.ContainsKey(driveName);
-        }
-
-        public IEnumerator<VirtualMemory> GetEnumerator() {
-            return _dic.Values.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() {
-            return _dic.Values.GetEnumerator();
-        }
-    }
-
     public class VirtualMemory : ViewModelBase {
         public static readonly VirtualMemory Empty = new VirtualMemory {
             DriveName = string.Empty,
