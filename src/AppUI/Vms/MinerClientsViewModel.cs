b88163824d58f0f46218500642980022fa8178da
ntminer <2362120103@qq.com>
2019-02-19 14:21:03 +0800

    ColumnsShow

--- src/AppUI/Vms/MinerClientsViewModel.cs
+++ src/AppUI/Vms/MinerClientsViewModel.cs
@@ -30,7 +30,19 @@ namespace NTMiner.Vms {
         private bool _minerIp = true;
         private bool _minerGroup = true;
         private bool _mainCoinCode = true;
-        private bool _mainCoinSpeed = true;
+        private bool _mainCoinSpeedText = true;
+        private bool _mainCoinWallet = true;
+        private bool _mainCoinPool = true;
+        private bool _kernel = true;
+        private bool _dualCoinCode = true;
+        private bool _dualCoinSpeedText = true;
+        private bool _dualCoinWallet = true;
+        private bool _dualCoinPool = true;
+        private bool _lastActivedOnText = true;
+        private bool _version = true;
+        private bool _remoteUserName = true;
+        private bool _remotePassword = true;
+        private bool _gpuInfo = true;
 
         public bool Work {
             get => _work;
@@ -74,11 +86,106 @@ namespace NTMiner.Vms {
             }
         }
 
-        public bool MainCoinSpeed {
-            get { return _mainCoinSpeed; }
+        public bool MainCoinSpeedText {
+            get { return _mainCoinSpeedText; }
             set {
-                _mainCoinSpeed = value;
-                OnPropertyChanged(nameof(MainCoinSpeed));
+                _mainCoinSpeedText = value;
+                OnPropertyChanged(nameof(MainCoinSpeedText));
+            }
+        }
+
+        public bool MainCoinWallet {
+            get => _mainCoinWallet;
+            set {
+                _mainCoinWallet = value;
+                OnPropertyChanged(nameof(MainCoinWallet));
+            }
+        }
+
+        public bool MainCoinPool {
+            get => _mainCoinPool;
+            set {
+                _mainCoinPool = value;
+                OnPropertyChanged(nameof(MainCoinPool));
+            }
+        }
+
+        public bool Kernel {
+            get => _kernel;
+            set {
+                _kernel = value;
+                OnPropertyChanged(nameof(Kernel));
+            }
+        }
+        public bool DualCoinCode {
+            get => _dualCoinCode;
+            set {
+                _dualCoinCode = value;
+                OnPropertyChanged(nameof(DualCoinCode));
+            }
+        }
+
+        public bool DualCoinSpeedText {
+            get => _dualCoinSpeedText;
+            set {
+                _dualCoinSpeedText = value;
+                OnPropertyChanged(nameof(DualCoinSpeedText));
+            }
+        }
+
+        public bool DualCoinWallet {
+            get => _dualCoinWallet;
+            set {
+                _dualCoinWallet = value;
+                OnPropertyChanged(nameof(DualCoinWallet));
+            }
+        }
+
+        public bool DualCoinPool {
+            get => _dualCoinPool;
+            set {
+                _dualCoinPool = value;
+                OnPropertyChanged(nameof(DualCoinPool));
+            }
+        }
+
+        public bool LastActivedOnText {
+            get => _lastActivedOnText;
+            set {
+                _lastActivedOnText = value;
+                OnPropertyChanged(nameof(LastActivedOnText));
+            }
+        }
+
+        public bool Version {
+            get => _version;
+            set {
+                _version = value;
+                OnPropertyChanged(nameof(Version));
+            }
+        }
+
+        public bool RemoteUserName {
+            get => _remoteUserName;
+            set {
+                _remoteUserName = value;
+                OnPropertyChanged(nameof(RemoteUserName));
+            }
+        }
+
+        public bool RemotePassword {
+            get => _remotePassword;
+            set {
+                _remotePassword = value;
+                OnPropertyChanged(nameof(RemotePassword));
+            }
+        }
+
+        public bool GpuInfo {
+            get => _gpuInfo;
+            set {
+                _gpuInfo = value;
+                OnPropertyChanged(nameof(GpuInfo));
             }
         }
     }
