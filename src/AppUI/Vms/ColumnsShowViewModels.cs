ad3486431f8a84e63d18c2312303993dc94360b7
ntminer <2362120103@qq.com>
2019-02-21 23:34:53 +0800

    test

--- src/AppUI/Vms/ColumnsShowViewModels.cs
+++ src/AppUI/Vms/ColumnsShowViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NTMiner.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Input;
@@ -14,6 +15,24 @@ namespace NTMiner.Vms {
             this.Add = new DelegateCommand(() => {
                 new ColumnsShowViewModel(Guid.NewGuid()).Edit.Execute(null);
             });
+            VirtualRoot.On<ColumnsShowAddedEvent>(
+                "添加了列显后刷新VM内存",
+                LogEnum.Console,
+                action: message => {
+
+                });
+            VirtualRoot.On<ColumnsShowUpdatedEvent>(
+                "更新了列显后刷新VM内存",
+                LogEnum.Console,
+                action: message => {
+
+                });
+            VirtualRoot.On<ColumnsShowRemovedEvent>(
+                "移除了列显后刷新VM内存",
+                LogEnum.Console,
+                action: message => {
+
+                });
             foreach (var item in NTMinerRoot.Current.ColumnsShowSet) {
                 _dicById.Add(item.GetId(), new ColumnsShowViewModel(item));
             }
