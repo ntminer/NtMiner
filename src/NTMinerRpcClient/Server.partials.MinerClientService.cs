8feaf4b001930d78a60e927f88af3f00d5729092
ntminer <2362120103@qq.com>
2019-02-24 00:19:33 +0800

    test

--- src/NTMinerRpcClient/Server.partials.MinerClientService.cs
+++ src/NTMinerRpcClient/Server.partials.MinerClientService.cs
@@ -11,10 +11,12 @@ namespace NTMiner {
             private MinerClientServiceFace() {
             }
 
-            public static void RestartWindowsAsync(string clientHost, Action<ResponseBase> callback) {
+            public static void RestartWindowsAsync(string clientIp, Action<ResponseBase> callback) {
                 Task.Factory.StartNew(() => {
                     try {
-                        RestartWindowsRequest request = new RestartWindowsRequest();
+                        RestartWindowsRequest request = new RestartWindowsRequest {
+                            ClientIp = clientIp
+                        };
                         ResponseBase response = Request<ResponseBase>("MinerClient", "RestartWindows", request);
                         callback?.Invoke(response);
                     }
@@ -25,10 +27,12 @@ namespace NTMiner {
                 });
             }
 
-            public static void ShutdownWindowsAsync(string clientHost, Action<ResponseBase> callback) {
+            public static void ShutdownWindowsAsync(string clientIp, Action<ResponseBase> callback) {
                 Task.Factory.StartNew(() => {
                     try {
-                        ShutdownWindowsRequest request = new ShutdownWindowsRequest();
+                        ShutdownWindowsRequest request = new ShutdownWindowsRequest {
+                            ClientIp = clientIp
+                        };
                         ResponseBase response = Request<ResponseBase>("MinerClient", "ShutdownWindows", request);
                         callback?.Invoke(response);
                     }
@@ -39,10 +43,13 @@ namespace NTMiner {
                 });
             }
 
-            public static void OpenNTMinerAsync(string clientHost, Action<ResponseBase> callback) {
+            public static void OpenNTMinerAsync(string clientIp, Guid workId, Action<ResponseBase> callback) {
                 Task.Factory.StartNew(() => {
                     try {
-                        OpenNTMinerRequest request = new OpenNTMinerRequest();
+                        OpenNTMinerRequest request = new OpenNTMinerRequest {
+                            ClientIp = clientIp,
+                            WorkId = workId
+                        };
                         ResponseBase response = Request<ResponseBase>("MinerClient", "OpenNTMiner", request);
                         callback?.Invoke(response);
                     }
@@ -53,10 +60,13 @@ namespace NTMiner {
                 });
             }
 
-            public static void RestartNTMinerAsync(string clientHost, Action<ResponseBase> callback) {
+            public static void RestartNTMinerAsync(string clientIp, Guid workId, Action<ResponseBase> callback) {
                 Task.Factory.StartNew(() => {
                     try {
-                        RestartNTMinerRequest request = new RestartNTMinerRequest();
+                        RestartNTMinerRequest request = new RestartNTMinerRequest {
+                            ClientIp = clientIp,
+                            WorkId = workId
+                        };
                         ResponseBase response = Request<ResponseBase>("MinerClient", "RestartNTMiner", request);
                         callback?.Invoke(response);
                     }
@@ -67,10 +77,13 @@ namespace NTMiner {
                 });
             }
 
-            public static void UpgradeNTMinerAsync(string clientHost, Action<ResponseBase> callback) {
+            public static void UpgradeNTMinerAsync(string clientIp, string ntminerFileName, Action<ResponseBase> callback) {
                 Task.Factory.StartNew(() => {
                     try {
-                        UpgradeNTMinerRequest request = new UpgradeNTMinerRequest();
+                        UpgradeNTMinerRequest request = new UpgradeNTMinerRequest {
+                            ClientIp = clientIp,
+                            NTMinerFileName = ntminerFileName
+                        };
                         ResponseBase response = Request<ResponseBase>("MinerClient", "UpgradeNTMiner", request);
                         callback?.Invoke(response);
                     }
@@ -81,10 +94,12 @@ namespace NTMiner {
                 });
             }
 
-            public static void CloseNTMinerAsync(string clientHost, Action<ResponseBase> callback) {
+            public static void CloseNTMinerAsync(string clientIp, Action<ResponseBase> callback) {
                 Task.Factory.StartNew(() => {
                     try {
-                        CloseNTMinerRequest request = new CloseNTMinerRequest();
+                        CloseNTMinerRequest request = new CloseNTMinerRequest {
+                            ClientIp = clientIp
+                        };
                         ResponseBase response = Request<ResponseBase>("MinerClient", "CloseNTMiner", request);
                         callback?.Invoke(response);
                     }
@@ -95,10 +110,15 @@ namespace NTMiner {
                 });
             }
 
-            public static void StartMineAsync(string clientHost, Action<ResponseBase> callback) {
+            public static void StartMineAsync(string clientIp, Guid workId, Action<ResponseBase> callback) {
                 Task.Factory.StartNew(() => {
                     try {
-                        StartMineRequest request = new StartMineRequest();
+                        StartMineRequest request = new StartMineRequest {
+                            ClientIp = clientIp,
+                            LoginName = LoginName,
+                            WorkId = workId
+                        };
+                        request.SignIt(PasswordSha1);
                         ResponseBase response = Request<ResponseBase>("MinerClient", "StartMine", request);
                         callback?.Invoke(response);
                     }
@@ -109,10 +129,14 @@ namespace NTMiner {
                 });
             }
 
-            public static void StopMineAsync(string clientHost, Action<ResponseBase> callback) {
+            public static void StopMineAsync(string clientIp, Action<ResponseBase> callback) {
                 Task.Factory.StartNew(() => {
                     try {
-                        StopMineRequest request = new StopMineRequest();
+                        StopMineRequest request = new StopMineRequest {
+                            ClientIp = clientIp,
+                            LoginName = LoginName
+                        };
+                        request.SignIt(PasswordSha1);
                         ResponseBase response = Request<ResponseBase>("MinerClient", "StopMine", request);
                         callback?.Invoke(response);
                     }
@@ -123,10 +147,16 @@ namespace NTMiner {
                 });
             }
 
-            public static void SetClientMinerProfilePropertyAsync(string clientHost, Action<ResponseBase> callback) {
+            public static void SetClientMinerProfilePropertyAsync(string clientIp, string propertyName, object value, Action<ResponseBase> callback) {
                 Task.Factory.StartNew(() => {
                     try {
-                        SetClientMinerProfilePropertyRequest request = new SetClientMinerProfilePropertyRequest();
+                        SetClientMinerProfilePropertyRequest request = new SetClientMinerProfilePropertyRequest {
+                            ClientIp = clientIp,
+                            LoginName = LoginName,
+                            PropertyName = propertyName,
+                            Value = value
+                        };
+                        request.SignIt(PasswordSha1);
                         ResponseBase response = Request<ResponseBase>("MinerClient", "SetClientMinerProfileProperty", request);
                         callback?.Invoke(response);
                     }
