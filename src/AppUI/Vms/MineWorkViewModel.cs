c97371055f40cde33286e998f1897b5972298d03
ntminer <2362120103@qq.com>
2019-02-24 19:36:30 +0800

    test

--- src/AppUI/Vms/MineWorkViewModel.cs
+++ src/AppUI/Vms/MineWorkViewModel.cs
@@ -50,7 +50,7 @@ namespace NTMiner.Vms {
                 }
                 else {
                     VirtualRoot.Execute(new AddMineWorkCommand(this));
-                    MineWorkEdit.ShowWindow(new MineWorkViewModel(this));
+                    this.Edit.Execute(null);
                 }
                 CloseWindow?.Invoke();
             });
