dc77fb4d9d99dd46a4b95e9236cb0f89d495cdd5
ntminer <2362120103@qq.com>
2019-02-24 13:08:26 +0800

    test

--- src/NTMiner.Core/Core/MinerServer/Impl/AppSettingSet.cs
+++ src/NTMiner.Core/Core/MinerServer/Impl/AppSettingSet.cs
@@ -8,6 +8,30 @@ namespace NTMiner.Core.MinerServer.Impl {
         private readonly INTMinerRoot _root;
         public AppSettingSet(INTMinerRoot root) {
             _root = root;
+            VirtualRoot.Accept<SetAppSettingCommand>(
+                "处理设置AppSetting命令",
+                LogEnum.Console,
+                action: message => {
+                    if (message.AppSetting == null) {
+                        return;
+                    }
+                    AppSettingData entity = AppSettingData.Create(message.AppSetting);
+                    Server.AppSettingService.SetAppSettingAsync(entity, response => {
+                        if (response.IsSuccess()) {
+                            AppSettingData item;
+                            if (_dicByKey.TryGetValue(message.AppSetting.Key, out item)) {
+                                item.Value = message.AppSetting.Value;
+                            }
+                            else {
+                                _dicByKey.Add(message.AppSetting.Key, entity);
+                            }
+                            VirtualRoot.Happened(new AppSettingChangedEvent(entity));
+                        }
+                        else if (response != null) {
+                            Write.UserLine(response.Description, System.ConsoleColor.Red);
+                        }
+                    });
+                });
         }
 
         private bool _isInited = false;
