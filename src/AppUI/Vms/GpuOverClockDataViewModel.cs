1c20fd25f42318526138a83a016da870ede7a195
ntminer <2362120103@qq.com>
2019-02-26 00:40:09 +0800

    overclock

---
+++ src/AppUI/Vms/GpuOverClockDataViewModel.cs
@@ -0,0 +1,77 @@
+﻿using NTMiner.Core.Gpus;
+
+namespace NTMiner.Vms {
+    public class GpuOverClockDataViewModel : ViewModelBase, IGpuOverClockData {
+        private int _index;
+        private string _name;
+        private int _coreClockDelta;
+        private int _memoryClockDelta;
+        private int _powerCapacity;
+        private int _cool;
+
+        public GpuOverClockDataViewModel(IGpuOverClockData data) {
+            _index = data.Index;
+            _name = data.Name;
+            _coreClockDelta = data.CoreClockDelta;
+            _memoryClockDelta = data.MemoryClockDelta;
+            _powerCapacity = data.PowerCapacity;
+            _cool = data.Cool;
+        }
+
+        public void Update(IGpuOverClockData data) {
+            this.Name = data.Name;
+            this.CoreClockDelta = data.CoreClockDelta;
+            this.MemoryClockDelta = data.MemoryClockDelta;
+            this.PowerCapacity = data.PowerCapacity;
+            this.Cool = data.Cool;
+        }
+
+        public int Index {
+            get => _index;
+            set {
+                _index = value;
+                OnPropertyChanged(nameof(Index));
+            }
+        }
+
+        public string Name {
+            get => _name;
+            set {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public int CoreClockDelta {
+            get => _coreClockDelta;
+            set {
+                _coreClockDelta = value;
+                OnPropertyChanged(nameof(CoreClockDelta));
+            }
+        }
+
+        public int MemoryClockDelta {
+            get => _memoryClockDelta;
+            set {
+                _memoryClockDelta = value;
+                OnPropertyChanged(nameof(MemoryClockDelta));
+            }
+        }
+
+        public int PowerCapacity {
+            get => _powerCapacity;
+            set {
+                _powerCapacity = value;
+                OnPropertyChanged(nameof(PowerCapacity));
+            }
+        }
+
+        public int Cool {
+            get => _cool;
+            set {
+                _cool = value;
+                OnPropertyChanged(nameof(Cool));
+            }
+        }
+    }
+}
---
+++ src/AppUI/Vms/GpuOverClockDataViewModels.cs
@@ -0,0 +1,37 @@
+﻿using NTMiner.Core;
+using NTMiner.Core.Gpus;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NTMiner.Vms {
+    public class GpuOverClockDataViewModels : ViewModelBase {
+        public static readonly GpuOverClockDataViewModels Current = new GpuOverClockDataViewModels();
+
+        private readonly Dictionary<int, GpuOverClockDataViewModel> _dicByIndex = new Dictionary<int, GpuOverClockDataViewModel>();
+
+        private GpuOverClockDataViewModels() {
+            VirtualRoot.On<GpuOverClockDataAddedOrUpdatedEvent>(
+                "添加或更新了Gpu超频数据后刷新VM内存",
+                LogEnum.Console,
+                action: message => {
+                    GpuOverClockDataViewModel vm;
+                    if (_dicByIndex.TryGetValue(message.Source.Index, out vm)) {
+                        vm.Update(message.Source);
+                    }
+                    else {
+                        _dicByIndex.Add(message.Source.Index, new GpuOverClockDataViewModel(message.Source));
+                    }
+                });
+            foreach (var gpu in NTMinerRoot.Current.GpuSet) {
+                IGpuOverClockData data = NTMinerRoot.Current.GpuOverClockDataSet.GetGpuOverClockData(gpu.Index);
+                _dicByIndex.Add(data.Index, new GpuOverClockDataViewModel(data));
+            }
+        }
+
+        public List<GpuOverClockDataViewModel> List {
+            get {
+                return _dicByIndex.Values.ToList();
+            }
+        }
+    }
+}
