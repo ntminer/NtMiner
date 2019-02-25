44394653221dd241d60997c662ea4494a91095cd
ntminer <2362120103@qq.com>
2019-02-26 01:23:37 +0800

    test

---
+++ src/AppUI/Views/Ucs/GpusOverClock.xaml.cs
@@ -0,0 +1,25 @@
+﻿using NTMiner.Vms;
+using System.Windows.Controls;
+
+namespace NTMiner.Views.Ucs {
+    public partial class GpusOverClock : UserControl {
+        public static void ShowWindow(string appType) {
+            ContainerWindow.ShowWindow(new ContainerWindowViewModel {
+                IconName = "Icon_GpuOverClock",
+                CloseVisible = System.Windows.Visibility.Visible,
+                FooterVisible = System.Windows.Visibility.Collapsed
+            }, ucFactory: (window) => new GpusOverClock(appType), fixedSize: true);
+        }
+
+        public GpusOverClockViewModel Vm {
+            get {
+                return (GpusOverClockViewModel)this.DataContext;
+            }
+        }
+
+        public GpusOverClock(string appType) {
+            InitializeComponent();
+            ResourceDictionarySet.Instance.FillResourceDic(this, this.Resources);
+        }
+    }
+}
---
+++ src/AppUI/Vms/GpusOverClockViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NTMiner.Vms {
+    public class GpusOverClockViewModel : ViewModelBase {
+        public static readonly GpusOverClockViewModel Current = new GpusOverClockViewModel();
+
+        private GpusOverClockViewModel() {
+
+        }
+    }
+}
