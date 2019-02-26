b4c73298f7947fed519347eb990199685c42e3af
ntminer <2362120103@qq.com>
2019-02-26 20:11:48 +0800

    test

--- src/NTMiner.Core/Core/Gpus/Impl/GpuAllOverClock.cs
+++ src/NTMiner.Core/Core/Gpus/Impl/GpuAllOverClock.cs
@@ -4,19 +4,27 @@
         }
 
         public void SetCool(IGpuOverClockData data) {
-            
+            foreach (var gpu in NTMinerRoot.Current.GpuSet) {
+                gpu.OverClock.SetCool(data);
+            }
         }
 
         public void SetCoreClock(IGpuOverClockData data) {
-            
+            foreach (var gpu in NTMinerRoot.Current.GpuSet) {
+                gpu.OverClock.SetCoreClock(data);
+            }
         }
 
         public void SetMemoryClock(IGpuOverClockData data) {
-            
+            foreach (var gpu in NTMinerRoot.Current.GpuSet) {
+                gpu.OverClock.SetMemoryClock(data);
+            }
         }
 
         public void SetPowerCapacity(IGpuOverClockData data) {
-            
+            foreach (var gpu in NTMinerRoot.Current.GpuSet) {
+                gpu.OverClock.SetPowerCapacity(data);
+            }
         }
     }
 }
