584cf9a6c172193ce9bba2b50ba93d69f492cf67
ntminer <2362120103@qq.com>
2019-02-28 02:20:13 +0800

    test

--- src/NTMiner.Core/Core/Gpus/Impl/NVIDIAClockDeltaSet.cs
+++ src/NTMiner.Core/Core/Gpus/Impl/NVIDIAClockDeltaSet.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace NTMiner.Core.Gpus.Impl {
     public class NVIDIAClockDeltaSet : IGpuClockDeltaSet {
@@ -23,7 +24,33 @@ namespace NTMiner.Core.Gpus.Impl {
         private void Init() {
             lock (_locker) {
                 if (!_isInited) {
-                    
+                    string commandLine = $"{SpecialPath.NTMinerOverClockFileFullName} gpu:{{0}} ps20e";
+                    const string coreClockDeltaPattern = @"c\[0\]\.freqDelta     = \d+ kHz \[(-?\d+) .. (\d+)\]";
+                    const string memoryClockDeltaPattern = @"c\[1\]\.freqDelta     = \d+ kHz \[(-?\d+) .. (\d+)\]";
+                    foreach (var gpu in _root.GpuSet) {
+                        if (gpu.Index == NTMinerRoot.GpuAllId) {
+                            continue;
+                        }
+                        int exitCode = -1;
+                        string output;
+                        Windows.Cmd.RunClose(SpecialPath.NTMinerOverClockFileFullName, $"gpu:{gpu.Index} ps20e", ref exitCode, out output);
+                        if (exitCode == 0) {
+                            Match match = Regex.Match(output, coreClockDeltaPattern);
+                            if (match.Success) {
+                                int coreClockDeltaMin;
+                                int coreClockDeltaMax;
+                                int.TryParse(match.Groups[1].Value, out coreClockDeltaMin);
+                                int.TryParse(match.Groups[2].Value, out coreClockDeltaMax);
+                            }
+                            match = Regex.Match(output, memoryClockDeltaPattern);
+                            if (match.Success) {
+                                int memoryClockDeltaMin;
+                                int memoryClockDeltaMax;
+                                int.TryParse(match.Groups[1].Value, out memoryClockDeltaMin);
+                                int.TryParse(match.Groups[2].Value, out memoryClockDeltaMax);
+                            }
+                        }
+                    }
                     _isInited = true;
                 }
             }
