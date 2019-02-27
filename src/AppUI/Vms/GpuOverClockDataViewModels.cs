d26d72690a9c1b66e3a26ad771e8511f2f28df78
ntminer <2362120103@qq.com>
2019-02-27 22:36:57 +0800

    test

--- src/AppUI/Vms/GpuOverClockDataViewModels.cs
+++ src/AppUI/Vms/GpuOverClockDataViewModels.cs
@@ -36,16 +36,6 @@ namespace NTMiner.Vms {
                         _gpuAllVmDicByCoinId.Add(coinId, vm);
                     }
                 }
-                for (int i = 1; i < 12; i++) {
-                    IGpuOverClockData data = NTMinerRoot.Current.GpuOverClockDataSet.GetGpuOverClockData(coinId, i);
-                    var vm = new GpuOverClockDataViewModel(data) {
-                        Name = "GPU"+ i
-                    };
-                    _dicById.Add(data.GetId(), vm);
-                    if (i == NTMinerRoot.GpuAllId) {
-                        _gpuAllVmDicByCoinId.Add(coinId, vm);
-                    }
-                }
             }
         }
 
