c67c1acf5b17eaea07bd0d016eef5c54d44da402
ntminer <2362120103@qq.com>
2019-02-27 22:04:52 +0800

    test

--- src/AppUI/Views/Ucs/GpusOverClock.xaml.cs
+++ src/AppUI/Views/Ucs/GpusOverClock.xaml.cs
@@ -18,8 +18,21 @@ namespace NTMiner.Views.Ucs {
 
         private void ScrollViewer_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
             if (e.LeftButton == MouseButtonState.Pressed && e.Source.GetType() == typeof(ScrollViewer)) {
+                ScrollViewer scrollViewer = (ScrollViewer)sender;
+                if (scrollViewer.ComputedVerticalScrollBarVisibility == Visibility.Visible) {
+                    Point p = e.GetPosition(scrollViewer);
+                    if (p.X > scrollViewer.ActualWidth - SystemParameters.ScrollWidth) {
+                        return;
+                    }
+                }
                 Window.GetWindow(this).DragMove();
                 e.Handled = true;
+            }
+        }
+
+        private void ItemsControl_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                Window.GetWindow(this).DragMove();
             }
         }
     }
