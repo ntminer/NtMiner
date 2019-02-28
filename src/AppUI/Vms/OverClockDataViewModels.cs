c49590c405a3c80e375b67a88c39231bfcaf701b
ntminer <2362120103@qq.com>
2019-02-28 22:22:52 +0800

    test

--- src/AppUI/Vms/OverClockDataViewModels.cs
+++ src/AppUI/Vms/OverClockDataViewModels.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using NTMiner.Core;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 
 namespace NTMiner.Vms {
-    public class OverClockDataViewModels : ViewModelBase {
+    public class OverClockDataViewModels : ViewModelBase, IEnumerable<OverClockDataViewModel> {
         public static readonly OverClockDataViewModels Current = new OverClockDataViewModels();
 
         private readonly Dictionary<Guid, OverClockDataViewModel> _dicById = new Dictionary<Guid, OverClockDataViewModel>();
@@ -20,6 +23,46 @@ namespace NTMiner.Vms {
             this.Add = new DelegateCommand(() => {
                 new OverClockDataViewModel(Guid.NewGuid()).Edit.Execute(null);
             });
+            VirtualRoot.On<OverClockDataAddedEvent>(
+                "添加超频建议后刷新VM内存",
+                LogEnum.Console,
+                action: message => {
+                    if (!_dicById.ContainsKey(message.Source.GetId())) {
+                        _dicById.Add(message.Source.GetId(), new OverClockDataViewModel(message.Source));
+                        OnPropertyChanged(nameof(List));
+                    }
+                });
+            VirtualRoot.On<OverClockDataUpdatedEvent>(
+                "更新超频建议后刷新VM内存",
+                LogEnum.Console,
+                action: message => {
+                    _dicById[message.Source.GetId()].Update(message.Source);
+                });
+            VirtualRoot.On<OverClockDataRemovedEvent>(
+                "删除超频建议后刷新VM内存",
+                LogEnum.Console,
+                action: message => {
+                    _dicById.Remove(message.Source.GetId());
+                    OnPropertyChanged(nameof(List));
+                });
+        }
+
+        public List<OverClockDataViewModel> List {
+            get {
+                return _dicById.Values.ToList();
+            }
+        }
+
+        public bool TryGetMineWorkVm(Guid id, out OverClockDataViewModel minerGroupVm) {
+            return _dicById.TryGetValue(id, out minerGroupVm);
+        }
+
+        public IEnumerator<OverClockDataViewModel> GetEnumerator() {
+            return _dicById.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return _dicById.Values.GetEnumerator();
         }
     }
 }
