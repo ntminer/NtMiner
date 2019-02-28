448eaebc1600c32893fde3e337bdef03a96f322b
ntminer <2362120103@qq.com>
2019-02-28 17:04:50 +0800

    test

---
+++ src/NTMinerDataObjects/MinerServer/OverClockData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NTMiner.MinerServer {
+    public class OverClockData : IOverClockData {
+        public OverClockData() { }
+
+        public Guid GetId() {
+            return this.Id;
+        }
+
+        public Guid Id { get; set; }
+
+        public Guid CoinId { get; set; }
+
+        public string Name { get; set; }
+
+        public int CoreClockDelta { get; set; }
+
+        public int MemoryClockDelta { get; set; }
+
+        public int PowerCapacity { get; set; }
+
+        public int Cool { get; set; }
+    }
+}
