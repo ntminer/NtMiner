4a8926363637e2d4a2731fa86b7cf1b595c3d3aa
ntminer <2362120103@qq.com>
2019-03-02 01:26:20 +0800

    test

--- src/NTMinerWpf/Converters/BoolInvertConverter.cs
+++ src/NTMinerWpf/Converters/BoolInvertConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace NTMiner.Converters {
     [ValueConversion(typeof(bool), typeof(bool))]
-    public class BooleanInvertConverter : IValueConverter {
+    public class BoolInvertConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             bool booleanValue = (bool)value;
             return !booleanValue;
