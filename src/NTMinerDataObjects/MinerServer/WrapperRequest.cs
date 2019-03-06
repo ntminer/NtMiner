7cfe8cea3dda523fcdde4141f70f9470a5bc39b2
ntminer <2362120103@qq.com>
2019-03-06 22:07:24 +0800

    test

---
+++ src/NTMinerDataObjects/MinerServer/WrapperRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace NTMiner.MinerServer {
+    public class WrapperRequest<T> : RequestBase, ISignatureRequest where T : ISignatureRequest {
+        public WrapperRequest() { }
+
+        public string LoginName { get; set; }
+
+        public T InnerRequest { get; set; }
+
+        public string Sign { get; set; }
+
+        public void SignIt(string password) {
+            this.Sign = this.GetSign(password);
+        }
+
+        public string GetSign(string password) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(nameof(MessageId)).Append(MessageId)
+                .Append(nameof(LoginName)).Append(LoginName)
+                .Append(nameof(InnerRequest)).Append(nameof(Sign)).Append(InnerRequest.Sign)
+                .Append(nameof(Timestamp)).Append(Timestamp.ToUlong())
+                .Append(nameof(UserData.Password)).Append(password);
+            return HashUtil.Sha1(sb.ToString());
+        }
+    }
+}
