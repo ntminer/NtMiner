b19bab1c60546807d6842606fc6cc9c775185c2c
ntminer <2362120103@qq.com>
2019-03-07 17:36:46 +0800

    test

--- src/AppUI/Views/Ucs/KernelPage.xaml.cs
+++ src/AppUI/Views/Ucs/KernelPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using NTMiner.Vms;
 using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace NTMiner.Views.Ucs {
@@ -7,9 +8,9 @@ namespace NTMiner.Views.Ucs {
         public static void ShowWindow(Guid kernelId, Action<bool, string> downloadComplete = null) {
             ContainerWindow.ShowWindow(new ContainerWindowViewModel {
                 IconName = "Icon_Logo",
-                CloseVisible = System.Windows.Visibility.Visible,
-                HeaderVisible = System.Windows.Visibility.Collapsed,
-                FooterVisible = System.Windows.Visibility.Collapsed,
+                CloseVisible = Visibility.Visible,
+                HeaderVisible = Visibility.Collapsed,
+                FooterVisible = Visibility.Collapsed,
                 Width = DevMode.IsDebugMode ? 1200 : AppStatic.MainWindowWidth,
                 Height = AppStatic.MainWindowHeight
             },
@@ -44,21 +45,27 @@ namespace NTMiner.Views.Ucs {
             ResourceDictionarySet.Instance.FillResourceDic(this, this.Resources);
         }
 
-        private void BtnDownloadMenu_Click(object sender, System.Windows.RoutedEventArgs e) {
-            if (Vm.KernelDownloadingVisible == System.Windows.Visibility.Visible) {
-                Vm.KernelDownloadingVisible = System.Windows.Visibility.Collapsed;
+        private void BtnDownloadMenu_Click(object sender, RoutedEventArgs e) {
+            if (Vm.KernelDownloadingVisible == Visibility.Visible) {
+                Vm.KernelDownloadingVisible = Visibility.Collapsed;
             }
             else {
-                Vm.KernelDownloadingVisible = System.Windows.Visibility.Visible;
+                Vm.KernelDownloadingVisible = Visibility.Visible;
             }
         }
 
-        private void ButtonLeftCoin_Click(object sender, System.Windows.RoutedEventArgs e) {
-            CoinsScrollView.PageLeft();
+        private void ButtonLeftCoin_Click(object sender, RoutedEventArgs e) {
+            double offset = CoinsScrollView.ContentHorizontalOffset - CoinsScrollView.ViewportWidth;
+            CoinsScrollView.ScrollToHorizontalOffset(offset);
+            ButtonLeft.IsEnabled = offset > 0;
+            ButtonRight.IsEnabled = true;
         }
 
-        private void ButtonRightCoin_Click(object sender, System.Windows.RoutedEventArgs e) {
-            CoinsScrollView.PageRight();
+        private void ButtonRightCoin_Click(object sender, RoutedEventArgs e) {
+            double offset = CoinsScrollView.ContentHorizontalOffset + CoinsScrollView.ViewportWidth;
+            CoinsScrollView.ScrollToHorizontalOffset(offset);
+            ButtonRight.IsEnabled = offset < CoinsScrollView.ScrollableWidth;
+            ButtonLeft.IsEnabled = true;
         }
     }
 }
