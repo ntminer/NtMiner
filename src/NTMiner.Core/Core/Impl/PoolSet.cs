d65d1ee5a7a3e1d02f6e7553ec8dc12d67bb1e9e
ntminer <2362120103@qq.com>
2019-03-10 02:06:26 +0800

    test

--- src/NTMiner.Core/Core/Impl/PoolSet.cs
+++ src/NTMiner.Core/Core/Impl/PoolSet.cs
@@ -137,10 +137,6 @@ namespace NTMiner.Core.Impl {
                 if (!_isInited) {
                     IEnumerable<PoolData> list;
                     if (VirtualRoot.IsControlCenter) {
-                        var repository = NTMinerRoot.CreateCompositeRepository<PoolData>(_isUseJson);
-                        list = repository.GetAll();
-                    }
-                    else {
                         var response = Server.ControlCenterService.GetPools();
                         if (response != null) {
                             list = response.Data;
@@ -148,6 +144,10 @@ namespace NTMiner.Core.Impl {
                         else {
                             list = new List<PoolData>();
                         }
+                    }
+                    else {
+                        var repository = NTMinerRoot.CreateCompositeRepository<PoolData>(_isUseJson);
+                        list = repository.GetAll();
                     }
                     foreach (var item in list) {
                         if (!_dicById.ContainsKey(item.GetId())) {
