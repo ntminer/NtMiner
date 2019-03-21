a9fd2baab26febb74339fbcb31c4abacf3269a24
ntminer <2362120103@qq.com>
2019-03-21 17:08:32 +0800

    test

--- src/AppUI/Vms/GpuProfilesPageViewModel.cs
+++ src/AppUI/Vms/GpuProfilesPageViewModel.cs
@@ -1,36 +1,31 @@
 ﻿using NTMiner.MinerServer;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace NTMiner.Vms {
     public class GpuProfilesPageViewModel : ViewModelBase {
-        private List<CoinViewModel> _coinVms;
-
         public GpuProfilesPageViewModel(IClientData client) {
             if (client != null) {
                 Client.MinerClientService.GetGpuProfilesJsonAsync(client.MinerIp, (data, e) => {
                     if (data != null) {
-                        List<CoinViewModel> coinVms = new List<CoinViewModel>();
                         foreach (var coinOverClock in data.CoinOverClocks) {
                             CoinViewModel coinVm;
                             if (CoinViewModels.Current.TryGetCoinVm(coinOverClock.CoinId, out coinVm)) {
                                 coinVm.IsOverClockEnabled = coinOverClock.IsOverClockEnabled;
                                 coinVm.IsOverClockGpuAll = coinOverClock.IsOverClockGpuAll;
-                                coinVms.Add(coinVm);
                             }
                         }
-                        this.CoinVms = coinVms;
-                        this.CurrentCoin = coinVms.FirstOrDefault();
+                        this.CurrentCoin = CoinVms.MainCoins.FirstOrDefault(a => a.IsOverClockEnabled);
+                        if (this.CurrentCoin == null) {
+                            this.CurrentCoin = CoinVms.MainCoins.FirstOrDefault();
+                        }
                     }
                 });
             }
         }
 
-        public List<CoinViewModel> CoinVms {
-            get => _coinVms;
-            set {
-                _coinVms = value;
-                OnPropertyChanged(nameof(CoinVms));
+        public CoinViewModels CoinVms {
+            get {
+                return CoinViewModels.Current;
             }
         }
 
