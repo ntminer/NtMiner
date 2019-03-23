75255ee219f3bcebcde389abacdcc3ce5558c497
ntminer <2362120103@qq.com>
2019-03-23 19:31:09 +0800

    test

--- src/AppUI/Vms/GpuSpeedDataViewModel.cs
+++ src/AppUI/Vms/GpuSpeedDataViewModel.cs
@@ -4,6 +4,8 @@ using System.Windows.Media;
 namespace NTMiner.Vms {
     public class GpuSpeedDataViewModel : ViewModelBase {
         private readonly GpuSpeedData _data;
+        private SolidColorBrush _temperatureForeground = MinerClientViewModel.DefaultForeground;
+
         public GpuSpeedDataViewModel(GpuSpeedData data) {
             _data = data;
         }
@@ -68,7 +70,6 @@ namespace NTMiner.Vms {
             }
         }
 
-        private SolidColorBrush _temperatureForeground = MinerClientViewModel.DefaultForeground;
         public SolidColorBrush TemperatureForeground {
             get {
                 return _temperatureForeground;
@@ -113,6 +114,66 @@ namespace NTMiner.Vms {
         public string PowerUsageWText {
             get {
                 return PowerUsageW.ToString("f0") + "W";
+            }
+        }
+
+        public int CoreClockDelta {
+            get => _data.CoreClockDelta;
+            set {
+                _data.CoreClockDelta = value;
+                OnPropertyChanged(nameof(CoreClockDelta));
+                OnPropertyChanged(nameof(CoreClockDeltaText));
+            }
+        }
+
+        public string CoreClockDeltaText {
+            get {
+                return CoreClockDelta.ToString() + "M";
+            }
+        }
+
+        public int MemoryClockDelta {
+            get => _data.MemoryClockDelta;
+            set {
+                _data.MemoryClockDelta = value;
+                OnPropertyChanged(nameof(MemoryClockDelta));
+                OnPropertyChanged(nameof(MemoryClockDeltaText));
+            }
+        }
+
+        public string MemoryClockDeltaText {
+            get {
+                return MemoryClockDelta.ToString() + "M";
+            }
+        }
+
+        public int Cool {
+            get => _data.Cool;
+            set {
+                _data.Cool = value;
+                OnPropertyChanged(nameof(Cool));
+                OnPropertyChanged(nameof(CoolText));
+            }
+        }
+
+        public string CoolText {
+            get {
+                return this.Cool.ToString() + "%";
+            }
+        }
+
+        public double Power {
+            get => _data.Power;
+            set {
+                _data.Power = value;
+                OnPropertyChanged(nameof(Power));
+                OnPropertyChanged(nameof(PowerText));
+            }
+        }
+
+        public string PowerText {
+            get {
+                return this.Power.ToString("f0") + "%";
             }
         }
     }
