afe0ff27255cf51e68201167d3f3c94a772e0bf2
ntminer <2362120103@qq.com>
2019-03-24 14:04:34 +0800

    test

--- src/AppUI/Vms/GpuProfilesPageViewModel.cs
+++ src/AppUI/Vms/GpuProfilesPageViewModel.cs
@@ -14,12 +14,16 @@ namespace NTMiner.Vms {
         private string _redText;
         private CoinViewModel _coinVm;
         private GpuProfilesJsonDb _data;
+        private bool _isEnabled;
 
         public ICommand Save { get; private set; }
 
         public Action CloseWindow { get; set; }
 
         public GpuProfilesPageViewModel(IClientData client) {
+            if (client == null) {
+                throw new ArgumentNullException(nameof(client));
+            }
             this.Save = new DelegateCommand(() => {
                 if (_data == null) {
                     return;
@@ -45,82 +49,89 @@ namespace NTMiner.Vms {
                 Client.NTMinerDaemonService.SaveGpuProfilesJsonAsync(client.MinerIp, json);
                 CloseWindow?.Invoke();
             });
-            if (client != null) {
-                Client.NTMinerDaemonService.GetGpuProfilesJsonAsync(client.MinerIp, (data, e) => {
-                    _data = data;
-                    if (e != null) {
-                        Write.UserLine(e.Message, System.ConsoleColor.Red);
+            Client.NTMinerDaemonService.GetGpuProfilesJsonAsync(client.MinerIp, (data, e) => {
+                _data = data;
+                if (e != null) {
+                    Write.UserLine(e.Message, System.ConsoleColor.Red);
+                }
+                else if (data != null) {
+                    IsEnabled = true;
+                    string iconName;
+                    switch (data.GpuType) {
+                        case GpuType.NVIDIA:
+                            iconName = "Icon_Nvidia";
+                            GpuIconFill = "Green";
+                            RedText = "超频有风险，操作需谨慎";
+                            break;
+                        case GpuType.AMD:
+                            iconName = "Icon_AMD";
+                            GpuIconFill = "Red";
+                            RedText = "暂不支持A卡超频";
+                            break;
+                        case GpuType.Empty:
+                        default:
+                            iconName = "Icon_GpuEmpty";
+                            GpuIconFill = "Gray";
+                            RedText = "挖矿端没有显卡";
+                            break;
                     }
-                    else if (data != null) {
-                        string iconName;
-                        switch (data.GpuType) {
-                            case GpuType.NVIDIA:
-                                iconName = "Icon_Nvidia";
-                                GpuIconFill = "Green";
-                                RedText = "超频有风险，操作需谨慎";
-                                break;
-                            case GpuType.AMD:
-                                iconName = "Icon_AMD";
-                                GpuIconFill = "Red";
-                                RedText = "暂不支持A卡超频";
-                                break;
-                            case GpuType.Empty:
-                            default:
-                                iconName = "Icon_GpuEmpty";
-                                GpuIconFill = "Gray";
-                                RedText = "挖矿端没有显卡";
-                                break;
+                    GpuIcon = (Geometry)System.Windows.Application.Current.Resources[iconName];
+                    foreach (var coinVm in CoinViewModels.Current.MainCoins) {
+                        var coinOverClock = data.CoinOverClocks.FirstOrDefault(a => a.CoinId == coinVm.Id);
+                        var gpuProfiles = data.GpuProfiles.Where(a => a.CoinId == coinVm.Id).ToArray();
+                        if (coinOverClock == null) {
+                            coinOverClock = new CoinOverClockData() {
+                                CoinId = coinVm.Id,
+                                IsOverClockEnabled = false,
+                                IsOverClockGpuAll = true
+                            };
                         }
-                        GpuIcon = (Geometry)System.Windows.Application.Current.Resources[iconName];
-                        foreach (var coinVm in CoinViewModels.Current.MainCoins) {
-                            var coinOverClock = data.CoinOverClocks.FirstOrDefault(a => a.CoinId == coinVm.Id);
-                            var gpuProfiles = data.GpuProfiles.Where(a => a.CoinId == coinVm.Id).ToArray();
-                            if (coinOverClock == null) {
-                                coinOverClock = new CoinOverClockData() {
-                                    CoinId = coinVm.Id,
-                                    IsOverClockEnabled = false,
-                                    IsOverClockGpuAll = true
-                                };
+                        coinVm.IsOverClockEnabled = coinOverClock.IsOverClockEnabled;
+                        coinVm.IsOverClockGpuAll = coinOverClock.IsOverClockGpuAll;
+                        List<GpuProfileViewModel> gpuProfileVms = new List<GpuProfileViewModel>();
+                        GpuProfileViewModel gpuAllProfileVm = null;
+                        #region
+                        foreach (var gpu in data.Gpus.OrderBy(a => a.Index)) {
+                            var gpuProfile = gpuProfiles.FirstOrDefault(a => a.Index == gpu.Index);
+                            if (gpuProfile == null) {
+                                gpuProfile = new GpuProfileData(coinVm.Id, gpu.Index);
                             }
-                            coinVm.IsOverClockEnabled = coinOverClock.IsOverClockEnabled;
-                            coinVm.IsOverClockGpuAll = coinOverClock.IsOverClockGpuAll;
-                            List<GpuProfileViewModel> gpuProfileVms = new List<GpuProfileViewModel>();
-                            GpuProfileViewModel gpuAllProfileVm = null;
-                            #region
-                            foreach (var gpu in data.Gpus.OrderBy(a => a.Index)) {
-                                var gpuProfile = gpuProfiles.FirstOrDefault(a => a.Index == gpu.Index);
-                                if (gpuProfile == null) {
-                                    gpuProfile = new GpuProfileData(coinVm.Id, gpu.Index);
-                                }
-                                var gpuVm = new GpuViewModel(gpu, data.Gpus);
-                                if (gpu.Index == NTMinerRoot.GpuAllId) {
-                                    gpuAllProfileVm = new GpuProfileViewModel(gpuProfile, gpuVm);
-                                }
-                                else {
-                                    gpuProfileVms.Add(new GpuProfileViewModel(gpuProfile, gpuVm));
-                                }
+                            var gpuVm = new GpuViewModel(gpu, data.Gpus);
+                            if (gpu.Index == NTMinerRoot.GpuAllId) {
+                                gpuAllProfileVm = new GpuProfileViewModel(gpuProfile, gpuVm);
                             }
-                            if (gpuAllProfileVm == null) {
-                                gpuAllProfileVm = new GpuProfileViewModel(
-                                    new GpuProfileData(coinVm.Id, NTMinerRoot.GpuAllId), new GpuViewModel(new GpuData {
-                                        Index = NTMinerRoot.GpuAllId,
-                                        Name = "All",
-                                        CoreClockDeltaMax = 0,
-                                        CoreClockDeltaMin = 0,
-                                        MemoryClockDeltaMax = 0,
-                                        MemoryClockDeltaMin = 0
-                                    }, data.Gpus));
+                            else {
+                                gpuProfileVms.Add(new GpuProfileViewModel(gpuProfile, gpuVm));
                             }
-                            #endregion
-                            coinVm.GpuAllProfileVm = gpuAllProfileVm;
-                            coinVm.GpuProfileVms = gpuProfileVms;
                         }
-                        this.CoinVm = CoinVms.MainCoins.FirstOrDefault(a => a.IsOverClockEnabled);
-                        if (this.CoinVm == null) {
-                            this.CoinVm = CoinVms.MainCoins.FirstOrDefault();
+                        if (gpuAllProfileVm == null) {
+                            gpuAllProfileVm = new GpuProfileViewModel(
+                                new GpuProfileData(coinVm.Id, NTMinerRoot.GpuAllId), new GpuViewModel(new GpuData {
+                                    Index = NTMinerRoot.GpuAllId,
+                                    Name = "All",
+                                    CoreClockDeltaMax = 0,
+                                    CoreClockDeltaMin = 0,
+                                    MemoryClockDeltaMax = 0,
+                                    MemoryClockDeltaMin = 0
+                                }, data.Gpus));
                         }
+                        #endregion
+                        coinVm.GpuAllProfileVm = gpuAllProfileVm;
+                        coinVm.GpuProfileVms = gpuProfileVms;
                     }
-                });
+                    this.CoinVm = CoinVms.MainCoins.FirstOrDefault(a => a.IsOverClockEnabled);
+                    if (this.CoinVm == null) {
+                        this.CoinVm = CoinVms.MainCoins.FirstOrDefault();
+                    }
+                }
+            });
+        }
+
+        public bool IsEnabled {
+            get => _isEnabled;
+            set {
+                _isEnabled = value;
+                OnPropertyChanged(nameof(IsEnabled));
             }
         }
 
