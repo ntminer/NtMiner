73b9324ed3a2018a0da403972ec6cede29d56239
ntminer <2362120103@qq.com>
2019-03-25 17:42:35 +0800

    别名

--- src/NTMinerlib/AssemblyInfo.cs
+++ src/NTMinerlib/AssemblyInfo.cs
@@ -4,7 +4,7 @@ namespace NTMiner {
     public static class AssemblyInfo {
         public const string Version = "2.0.0";
         public const string Copyright = "Copyright ©  2019";
-        public const string Tag = "谜麟";
+        public const string Tag = "蛮吉";
         public static readonly string ServerJsonFileName = $"server{Version}.json";
         public static readonly string LangJsonFileName = $"lang{Version}.json";
         public static string ServerVersionJsonFileFullName = Path.Combine(VirtualRoot.GlobalDirFullName, ServerJsonFileName);
