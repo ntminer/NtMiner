0b4ba98e0815224a4a0aa3a2675d74be35396baf
ntminer <2362120103@qq.com>
2019-04-05 13:11:13 +0800

    cuda版本

---
+++ src/NTMiner.Core/Core/Gpus/Impl/Nvidia/ICudaVersion.cs
@@ -0,0 +1,7 @@
+﻿namespace NTMiner.Core.Gpus.Impl.Nvidia {
+    public interface ICudaVersion : IDbEntity<string> {
+        string Description { get; }
+        double MinDriverVersion { get; }
+        string Version { get; }
+    }
+}
