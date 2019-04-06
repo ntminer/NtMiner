2fd6327f890fd2d314dd3561a4a03ed546a3e967
ntminer <2362120103@qq.com>
2019-04-06 17:13:35 +0800

    添加注释

--- src/NTMiner.Core/Core/Gpus/Impl/TempGruarder.cs
+++ src/NTMiner.Core/Core/Gpus/Impl/TempGruarder.cs
@@ -10,8 +10,10 @@ namespace NTMiner.Core.Gpus.Impl {
         private TempGruarder() { }
 
         private bool _isInited = false;
-        private Dictionary<int, uint> _temperatureDic = new Dictionary<int, uint>();
-        private Dictionary<int, DateTime> _guardOn = new Dictionary<int, DateTime>();
+        // 记录显卡的上一次温度
+        private Dictionary<int, uint> _preTempDic = new Dictionary<int, uint>();
+        // 记录显卡温度抵达防御温度的时间
+        private Dictionary<int, DateTime> _fightedOnDic = new Dictionary<int, DateTime>();
         private readonly int _fanSpeedDownSeconds = 20;
         private readonly uint _fanSpeedDownStep = 2;
         private static readonly int _guardTemp = 60;
@@ -26,38 +28,44 @@ namespace NTMiner.Core.Gpus.Impl {
                     if (gpu.Index == NTMinerRoot.GpuAllId || root.MinerProfile.CoinId == Guid.Empty) {
                         return;
                     }
-                    IGpuProfile gpuProfile = GpuProfileSet.Instance.GetGpuProfile(root.MinerProfile.CoinId, gpu.Index);
-                    if (!gpuProfile.IsAutoFanSpeed) {
+                    IGpuProfile gpuProfile;
+                    if (GpuProfileSet.Instance.IsOverClockGpuAll(root.MinerProfile.CoinId)) {
                         gpuProfile = GpuProfileSet.Instance.GetGpuProfile(root.MinerProfile.CoinId, NTMinerRoot.GpuAllId);
-                        if (!gpuProfile.IsAutoFanSpeed) {
-                            return;
-                        }
                     }
-                    // 警戒时间
-                    DateTime guardOn;
-                    if (!_guardOn.TryGetValue(gpu.Index, out guardOn)) {
-                        guardOn = DateTime.Now;
-                        _guardOn.Add(gpu.Index, guardOn);
+                    else {
+                        gpuProfile = GpuProfileSet.Instance.GetGpuProfile(root.MinerProfile.CoinId, gpu.Index);
+                    }
+                    if (!gpuProfile.IsAutoFanSpeed) {
+                        return;
+                    }
+                    // 显卡温度抵达防御温度的时间
+                    DateTime fightedOn;
+                    if (!_fightedOnDic.TryGetValue(gpu.Index, out fightedOn)) {
+                        fightedOn = DateTime.Now;
+                        _fightedOnDic.Add(gpu.Index, fightedOn);
                     }
                     if (gpu.FanSpeed == 100) {
                         Write.DevDebug($"GPU{gpu.Index} 温度{gpu.Temperature}大于防线温度{_guardTemp}，但风扇转速已达100%");
                     }
                     else if (gpu.Temperature < _guardTemp) {
-                        if (!_temperatureDic.ContainsKey(gpu.Index)) {
-                            _temperatureDic.Add(gpu.Index, 0);
+                        if (!_preTempDic.ContainsKey(gpu.Index)) {
+                            _preTempDic.Add(gpu.Index, 0);
                         }
-                        if (gpu.Temperature > _temperatureDic[gpu.Index]) {
-                            _guardOn[gpu.Index] = DateTime.Now;
+                        // 如果当前温度不比上次的温度小
+                        if (gpu.Temperature >= _preTempDic[gpu.Index]) {
+                            fightedOn = DateTime.Now;
+                            _fightedOnDic[gpu.Index] = fightedOn;
                         }
-                        _temperatureDic[gpu.Index] = gpu.Temperature;
-                        // 连续?分钟GPU温度没有突破防线
-                        if (guardOn.AddSeconds(_fanSpeedDownSeconds) < DateTime.Now) {
+                        _preTempDic[gpu.Index] = gpu.Temperature;
+                        // 如果距离抵达防御温度的时间已经很久了则降速风扇
+                        if (fightedOn.AddSeconds(_fanSpeedDownSeconds) < DateTime.Now) {
                             int cool = (int)(gpu.FanSpeed - _fanSpeedDownStep);
+                            // 如果温度低于50度则直接将风扇设为驱动默认的最小转速
                             if (gpu.Temperature < 50) {
                                 cool = gpu.CoolMin;
                             }
                             if (cool >= gpu.CoolMin) {
-                                _guardOn[gpu.Index] = DateTime.Now;
+                                _fightedOnDic[gpu.Index] = DateTime.Now;
                                 root.GpuSet.OverClock.SetCool(gpu.Index, cool);
                                 Write.DevDebug($"GPU{gpu.Index} 风扇转速由{gpu.FanSpeed}%调低至{cool}%");
                             }
@@ -67,8 +75,8 @@ namespace NTMiner.Core.Gpus.Impl {
                         uint cool;
                         uint len;
                         // 防线突破可能是由于小量降低风扇转速造成的
-                        if (guardOn.AddSeconds(_fanSpeedDownSeconds) < DateTime.Now) {
-                            _guardOn[gpu.Index] = DateTime.Now;
+                        if (fightedOn.AddSeconds(_fanSpeedDownSeconds) < DateTime.Now) {
+                            _fightedOnDic[gpu.Index] = DateTime.Now;
                             len = 100 - gpu.FanSpeed;
                         }
                         else {
