87ffec18e440ac1641c1a719a2cf5199d769c063
ntminer <2362120103@qq.com>
2019-04-07 17:27:55 +0800

    不展示不支持当前显卡的内核

--- src/AppUI/Vms/CoinKernelSelectViewModel.cs
+++ src/AppUI/Vms/CoinKernelSelectViewModel.cs
@@ -57,7 +57,7 @@ namespace NTMiner.Vms {
 
         public List<CoinKernelViewModel> QueryResults {
             get {
-                IQueryable<CoinKernelViewModel> query = Coin.CoinKernels.Where(a => a.Kernel != null).OrderBy(a => a.SortNumber).AsQueryable();
+                IQueryable<CoinKernelViewModel> query = Coin.CoinKernels.Where(a => a.Kernel != null && a.IsSupported).OrderBy(a => a.SortNumber).AsQueryable();
                 if (!AppStatic.IsDebugMode) {
                     query = query.Where(a => a.Kernel.PublishState == PublishStatus.Published);
                 }
