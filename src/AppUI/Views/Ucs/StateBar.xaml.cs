3fd2b0c5349ae90237c813766b06f846de589b17
ntminer <2362120103@qq.com>
2019-04-18 00:41:15 +0800

    如果没有20系列的显卡，挖矿建议使用3xx驱动

--- src/AppUI/Views/Ucs/StateBar.xaml.cs
+++ src/AppUI/Views/Ucs/StateBar.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using NTMiner.Vms;
+using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -14,9 +15,37 @@ namespace NTMiner.Views.Ucs {
 
         public StateBar() {
             InitializeComponent();
-            if (NTMinerRoot.OSVirtualMemoryMb < NTMinerRoot.Current.GpuSet.Count * 4) {
+            var gpuSet = NTMinerRoot.Current.GpuSet;
+            if (NTMinerRoot.OSVirtualMemoryMb < gpuSet.Count * 4) {
                 BtnShowVirtualMemory.Foreground = new SolidColorBrush(Colors.Red);
             }
+            if (gpuSet.GpuType == GpuType.NVIDIA) {
+                if (gpuSet.All(a => !Is20NCard(a.Name))) {
+                    string nvDriverVersion = string.Empty;
+                    var driverVersion = gpuSet.Properties.FirstOrDefault(a => a.Code == "DriverVersion");
+                    if (driverVersion != null && driverVersion.Value != null) {
+                        nvDriverVersion = driverVersion.Value.ToString();
+                    }
+                    double driverNum;
+                    if (double.TryParse(nvDriverVersion, out driverNum) && driverNum >= 400) {
+                        TextBlockDriverVersion.Foreground = new SolidColorBrush(Colors.Red);
+                        TextBlockDriverVersion.ToolTip = "如果没有20系列的显卡，挖矿建议使用3xx驱动。";
+                    }
+                }
+            }
+        }
+
+        private static bool Is20NCard(string cardName) {
+            if (string.IsNullOrEmpty(cardName)) {
+                return false;
+            }
+            string[] nv20Cards = new string[] { "2060", "2070", "2080" };
+            foreach (var nv20 in nv20Cards) {
+                if (cardName.Contains(nv20)) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
