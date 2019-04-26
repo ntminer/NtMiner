328d67306cf2c354b87dac80beec09b0cbdb705b
ntminer <2362120103@qq.com>
2019-04-26 13:02:59 +0800

    60100

--- src/NTMinerlib/AssemblyInfo.cs
+++ src/NTMinerlib/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 namespace NTMiner {
     public static class AssemblyInfo {
         public const string Version = "2.0.0";
-        public const string Build = "60000";
+        public const string Build = "60100";
         public const string Copyright = "Copyright ©  2019";
         public const string Tag = "蛮吉";
         public static readonly string ServerJsonFileName = $"server{Version}.json";
