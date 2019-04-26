3dd5339256935f5c41a11e8c0e5db0477088f8df
ntminer <2362120103@qq.com>
2019-04-26 11:46:52 +0800

    按钮

--- src/AppUI/Views/Ucs/StartStopMineButton.xaml.cs
+++ src/AppUI/Views/Ucs/StartStopMineButton.xaml.cs
@@ -1,8 +1,28 @@
 ﻿using NTMiner.Vms;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace NTMiner.Views.Ucs {
     public partial class StartStopMineButton : UserControl {
+        [Description("开始按钮背景色"), Category("KbSkin")]
+        public SolidColorBrush StartButtonBackground {
+            get { return (SolidColorBrush)GetValue(StartButtonBackgroundProperty); }
+            set { SetValue(StartButtonBackgroundProperty, value); }
+        }
+        public static readonly DependencyProperty StartButtonBackgroundProperty =
+            DependencyProperty.Register("StartButtonBackground", typeof(SolidColorBrush), typeof(StartStopMineButton), new PropertyMetadata(new SolidColorBrush(Colors.Transparent)));
+
+
+        [Description("开始按钮前景色"), Category("KbSkin")]
+        public SolidColorBrush StartButtonForeground {
+            get { return (SolidColorBrush)GetValue(StartButtonForegroundProperty); }
+            set { SetValue(StartButtonForegroundProperty, value); }
+        }
+        public static readonly DependencyProperty StartButtonForegroundProperty =
+            DependencyProperty.Register("StartButtonForeground", typeof(SolidColorBrush), typeof(StartStopMineButton), new PropertyMetadata(new SolidColorBrush(Colors.Transparent)));
+
         private StartStopMineButtonViewModel Vm {
             get {
                 return MainWindowViewModel.Current.StartStopMineButtonVm;
