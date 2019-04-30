49e2d0b72f0b007cc4533db61e7e6745a1fe3d37
ntminer <2362120103@qq.com>
2019-04-30 11:38:04 +0800

    test

--- src/MinerClient/App.xaml.cs
+++ src/MinerClient/App.xaml.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Threading;
 
 namespace NTMiner {
     public partial class App : Application, IDisposable {
