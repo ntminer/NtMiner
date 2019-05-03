d74362ff91c45a227d43e79417053dae09202f9c
ntminer <2362120103@qq.com>
2019-05-03 22:14:53 +0800

    test

--- src/NTMinerlib/AssemblyInfo.cs
+++ src/NTMinerlib/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 
 namespace NTMiner {
     public static class AssemblyInfo {
-        public const string Version = "2.0.0";
-        public const string Build = "60100";
+        public const string Version = "2.1.0";
+        public const string Build = "*";
         public const string Copyright = "Copyright ©  2019";
         public const string Tag = "蛮吉";
-        public static readonly string ServerJsonFileName = $"server{Version}.json";
+        public static readonly string ServerJsonFileName = $"server2.0.0.json";
         public static string ServerVersionJsonFileFullName = Path.Combine(VirtualRoot.GlobalDirFullName, ServerJsonFileName);
         public static string OfficialServerHost = "server.ntminer.com";
         public static readonly string MinerJsonBucket = "https://minerjson.oss-cn-beijing.aliyuncs.com/";
