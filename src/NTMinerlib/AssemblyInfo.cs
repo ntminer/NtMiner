a1d8e770a82424e08ec743c1ce3e12221c810399
ntminer <2362120103@qq.com>
2019-05-09 11:39:04 +0800

    支持矿池品牌版

--- src/NTMinerlib/AssemblyInfo.cs
+++ src/NTMinerlib/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 namespace NTMiner {
     public static class AssemblyInfo {
         public const string Version = "2.1.0";
-        public const string Build = "50001";
+        public const string Build = "50002";
         public const string Copyright = "Copyright ©  2019";
         public const string Tag = "蛮吉";
         public static readonly string ServerJsonFileName = $"server2.0.0.json";
