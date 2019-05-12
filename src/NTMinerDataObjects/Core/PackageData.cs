dc5040ec4651131b7f14d9f68783cd1cb6614428
ntminer <2362120103@qq.com>
2019-05-12 08:42:43 +0800

    Package

---
+++ src/NTMinerDataObjects/Core/PackageData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NTMiner.Core {
+    public class PackageData : IPackage, IDbEntity<Guid> {
+        public Guid GetId() {
+            return this.Id;
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Guid> AlgoIds { get; set; }
+    }
+}
