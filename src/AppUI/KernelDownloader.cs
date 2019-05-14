f6545d1eba203410d53b0518cfaec44a0865cb7d
ntminer <2362120103@qq.com>
2019-05-14 14:03:15 +0800

    test

--- src/AppUI/KernelDownloader.cs
+++ src/AppUI/KernelDownloader.cs
@@ -4,6 +4,9 @@ using System;
 
 namespace NTMiner {
     public class KernelDownloader : IKernelDownloader {
+        public KernelDownloader() {
+        }
+
         public void Download(Guid kernelId, Action<bool, string> downloadComplete) {
             UIThread.Execute(() => {
                 KernelsWindow.ShowWindow(kernelId, downloadComplete);
