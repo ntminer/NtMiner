de4ffeda58ab575be431a0c8bc7bcda6df510433
kuibug <kuibug@outlook.com>
2019-05-15 18:49:17 +0800

    test

--- src/AppModels/View/AppViewFactory.cs
+++ src/AppModels/View/AppViewFactory.cs
@@ -1,14 +1,27 @@
-﻿using NTMiner;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
+using System.Reflection;
 
 namespace NTMiner.View {
     public static class AppViewFactory {
+        private static IAppViewFactory _instance = null;
+        private static readonly object _locker = new object();
         public static IAppViewFactory Instance {
             get {
-                throw new NotImplementedException();
+                if (_instance == null) {
+                    lock (_locker) {
+                        if (_instance == null) {
+                            Type type = typeof(AppViewFactory);
+                            var assembly = type.Assembly;
+                            using (var stream = assembly.GetManifestResourceStream(type, "AppViews2.dll")) {
+                                byte[] data = new byte[stream.Length];
+                                stream.Read(data, 0, data.Length);
+                                assembly = Assembly.Load(data);
+                                _instance = (IAppViewFactory)assembly.CreateInstance("NTMiner.AppViewFactory");
+                            }
+                        }
+                    }
+                }
+                return _instance;
             }
         }
     }
