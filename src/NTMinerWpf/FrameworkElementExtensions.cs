1d88476c65c78e2794b99e53a691ab7866d4ed04
kuibug <kuibug@outlook.com>
2019-05-15 17:40:47 +0800

    test

--- src/NTMinerWpf/FrameworkElementExtensions.cs
+++ src/NTMinerWpf/FrameworkElementExtensions.cs
@@ -3,12 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Windows;
 
-namespace NTMiner.Views {
+namespace NTMiner {
     public static class FrameworkElementExtensions {
         /// <summary>
         /// 命令窗口。使用该方法的代码行应将前两个参数放在第一行以方便vs查找引用时展示出参数信息
         /// </summary>
-        internal static DelegateHandler<TCmd> Window<TCmd>(this FrameworkElement uiElement, string description, LogEnum logType, Action<TCmd> action)
+        public static DelegateHandler<TCmd> Window<TCmd>(this FrameworkElement uiElement, string description, LogEnum logType, Action<TCmd> action)
             where TCmd : ICmd {
             if (uiElement.Resources == null) {
                 uiElement.Resources = new ResourceDictionary();
@@ -33,7 +33,7 @@ namespace NTMiner.Views {
         /// <summary>
         /// 事件响应
         /// </summary>
-        internal static DelegateHandler<TEvent> On<TEvent>(this FrameworkElement uiElement, string description, LogEnum logType, Action<TEvent> action)
+        public static DelegateHandler<TEvent> On<TEvent>(this FrameworkElement uiElement, string description, LogEnum logType, Action<TEvent> action)
             where TEvent : IEvent {
             if (uiElement.Resources == null) {
                 uiElement.Resources = new ResourceDictionary();
