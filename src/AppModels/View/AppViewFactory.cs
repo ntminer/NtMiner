38edaa36d5ebb5d801f6e8acc550dde919293011
kuibug <kuibug@outlook.com>
2019-05-16 10:40:44 +0800

    test

--- src/AppModels/View/AppViewFactory.cs
+++ src/AppModels/View/AppViewFactory.cs
@@ -1,18 +1,26 @@
 ﻿using NTMiner.MinerServer;
 using System.Reflection;
+using System.Text;
 using System.Windows;
 
 namespace NTMiner.View {
     public static class AppViewFactory {
         private static IAppViewFactory _innerFactory;
         private static readonly object _locker = new object();
+        // 这里自定义了视图程序集的加载
         private static IAppViewFactory InnerFactory {
             get {
                 if (_innerFactory == null) {
                     lock (_locker) {
                         if (_innerFactory == null) {
+                            string appViewsName = "AppViews1";
                             var assembly = Assembly.GetEntryAssembly();
-                            using (var stream = assembly.GetManifestResourceStream("NTMiner.Views.AppViews1.dll")) {
+                            using (var stream = assembly.GetManifestResourceStream("NTMiner.Views.AppViewsName=")) {
+                                byte[] data = new byte[stream.Length];
+                                stream.Read(data, 0, data.Length);
+                                appViewsName = Encoding.UTF8.GetString(data);
+                            }
+                            using (var stream = assembly.GetManifestResourceStream($"NTMiner.Views.{appViewsName}.dll")) {
                                 byte[] data = new byte[stream.Length];
                                 stream.Read(data, 0, data.Length);
                                 assembly = Assembly.Load(data);
