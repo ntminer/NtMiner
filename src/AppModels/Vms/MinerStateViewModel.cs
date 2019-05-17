cc3cd99f891113e66b79f7b14637f3be3185a51e
kuibug <kuibug@outlook.com>
2019-05-17 19:10:46 +0800

    test

---
+++ src/AppModels/Vms/MinerStateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NTMiner.Vms {
+    public class MinerStateViewModel : ViewModelBase {
+        public MinerStateViewModel() {
+            this.StateBarVm = new StateBarViewModel();
+        }
+
+        public StateBarViewModel StateBarVm {
+            get; private set;
+        }
+    }
+}
---
+++ src/AppViews1/Ucs/MinerState.xaml.cs
@@ -0,0 +1,37 @@
+﻿using NTMiner.Vms;
+using System;
+using System.Windows.Controls;
+
+namespace NTMiner.Views.Ucs {
+    public partial class MinerState : UserControl {
+        private MinerStateViewModel Vm {
+            get {
+                return (MinerStateViewModel)this.DataContext;
+            }
+        }
+
+        public MinerState() {
+            InitializeComponent();
+            this.On<Per1SecondEvent>("挖矿计时秒表", LogEnum.None,
+                action: message => {
+                    DateTime now = DateTime.Now;
+                    Vm.StateBarVm.BootTimeSpan = now - NTMinerRoot.Instance.CreatedOn;
+                    if (NTMinerRoot.IsAutoStart && VirtualRoot.SecondCount <= 10 && !NTMinerRoot.IsAutoStartCanceled) {
+                        return;
+                    }
+                    var mineContext = NTMinerRoot.Instance.CurrentMineContext;
+                    if (mineContext != null) {
+                        Vm.StateBarVm.MineTimeSpan = now - mineContext.CreatedOn;
+                        if (!Vm.StateBarVm.MinerProfile.IsMining) {
+                            Vm.StateBarVm.MinerProfile.IsMining = true;
+                        }
+                    }
+                    else {
+                        if (Vm.StateBarVm.MinerProfile.IsMining) {
+                            Vm.StateBarVm.MinerProfile.IsMining = false;
+                        }
+                    }
+                });
+        }
+    }
+}
