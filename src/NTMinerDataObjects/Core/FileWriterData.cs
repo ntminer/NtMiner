dac9aa3b041abb3c4136369980241dfc314148db
kuibug <kuibug@outlook.com>
2019-05-22 10:40:04 +0800

    test

--- src/NTMinerDataObjects/Core/FileWriterData.cs
+++ src/NTMinerDataObjects/Core/FileWriterData.cs
@@ -4,16 +4,14 @@ namespace NTMiner.Core {
     public class FileWriterData : IDbEntity<Guid>, IFileWriter {
         public FileWriterData() { }
 
-        public Guid Id { get; set; }
-
         public Guid GetId() {
             return this.Id;
         }
 
+        public Guid Id { get; set; }
+
         public string FileUrl { get; set; }
 
-        public string Boday { get; set; }
-
-        public string[] Parameters { get; set; }
+        public string Body { get; set; }
     }
 }
--- src/NTMinerDataObjects/Core/IFileWriter.cs
+++ src/NTMinerDataObjects/Core/IFileWriter.cs
@@ -8,13 +8,9 @@ namespace NTMiner.Core {
         string FileUrl { get; }
 
         /// <summary>
-        /// 该字符串里面可能具有变量，比如{pool1}、{wallet1}、{userName1}等
+        /// 该字符串里面往往具有变量，比如{pool}、{host}、{port}、{wallet}、{userName}、{pool1}、{wallet1}、{userName1}等。
+        /// 运行时会识别出里面的形参，并且只有当Body中所有的形参都被填充了时才会执行。
         /// </summary>
-        string Boday { get; }
-
-        /// <summary>
-        /// 形式参数。只有形式参数匹配时该文件写作者才会执行。
-        /// </summary>
-        string[] Parameters { get; }
+        string Body { get; }
     }
 }
