1135a342cccaca90244c907654050b29b7fa78bb
kuibug <kuibug@outlook.com>
2019-05-22 22:00:09 +0800

    test

--- src/AppModels/Vms/MinerStateViewModel.cs
+++ src/AppModels/Vms/MinerStateViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace NTMiner.Vms {
+﻿using System;
+
+namespace NTMiner.Vms {
     public class MinerStateViewModel : ViewModelBase {
         public const double DefaultSideWidth = 220;
         private double _sideWidth = DefaultSideWidth;
+
+        public MinerStateViewModel() {
+            if (!Design.IsInDesignMode) {
+                throw new InvalidProgramException();
+            }
+            this.StateBarVm = new StateBarViewModel();
+        }
 
         public MinerStateViewModel(StateBarViewModel stateBarVm) {
             this.StateBarVm = stateBarVm;
