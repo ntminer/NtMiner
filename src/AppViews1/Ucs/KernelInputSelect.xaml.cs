4510f38bb99c5c0730a49ccdfea68cbbc280a138
kuibug <kuibug@outlook.com>
2019-05-23 20:58:16 +0800

    test

---
+++ src/AppViews1/Ucs/KernelInputSelect.xaml.cs
@@ -0,0 +1,32 @@
+﻿using NTMiner.Vms;
+using System.Windows.Controls;
+
+namespace NTMiner.Views.Ucs {
+    public partial class KernelInputSelect : UserControl {
+        public KernelInputSelectViewModel Vm {
+            get {
+                return (KernelInputSelectViewModel)this.DataContext;
+            }
+        }
+
+        public KernelInputSelect(KernelInputSelectViewModel vm) {
+            this.DataContext = vm;
+            InitializeComponent();
+        }
+
+        private void KbButtonManagePools_Click(object sender, System.Windows.RoutedEventArgs e) {
+            Vm.HideView?.Execute(null);
+        }
+
+        private void DataGrid_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            Vm.OnOk?.Invoke(Vm.SelectedResult);
+        }
+
+        private void DataGrid_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+            if (e.Key == System.Windows.Input.Key.Enter) {
+                Vm.OnOk?.Invoke(Vm.SelectedResult);
+                e.Handled = true;
+            }
+        }
+    }
+}
