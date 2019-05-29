85a2e3d403c5e6bc2c963a6c501d33c2bd6a1808
kuibug <kuibug@outlook.com>
2019-05-29 13:31:23 +0800

    test

--- src/NTMiner.Core/DoubleExtension.cs
+++ src/NTMiner.Core/DoubleExtension.cs
@@ -3,19 +3,31 @@
 namespace NTMiner {
     public static class DoubleExtension {
         public static string ToUnitSpeedText(this double speed) {
+            ToUnitSpeedText(speed, out string speedValueText, out string speedUnit);
+            return $"{speedValueText} {speedUnit}";
+        }
+
+        public static void ToUnitSpeedText(this double speed, out string speedValueText, out string speedUnit) {
             if (speed >= 10000000000000) {
-                return (speed / 1000000000000d).ToString("f1") + " Th/s";
+                speedValueText = (speed / 1000000000000d).ToString("f1");
+                speedUnit = "Th/s";
             }
-            if (speed >= 10000000000) {
-                return (speed / 1000000000d).ToString("f1") + " Gh/s";
+            else if (speed >= 10000000000) {
+                speedValueText = (speed / 1000000000d).ToString("f1");
+                speedUnit = "Gh/s";
             }
-            if (speed >= 10000000) {
-                return (speed / 1000000d).ToString("f1") + " Mh/s";
+            else if (speed >= 10000000) {
+                speedValueText = (speed / 1000000d).ToString("f1");
+                speedUnit = "Mh/s";
             }
-            if (speed >= 10000) {
-                return (speed / 1000d).ToString("f1") + " Kh/s";
+            else if (speed >= 10000) {
+                speedValueText = (speed / 1000d).ToString("f1");
+                speedUnit = "Kh/s";
             }
-            return speed.ToString("f2") + " H/s";
+            else {
+                speedValueText = speed.ToString("f2");
+                speedUnit = "H/s";
+            }
         }
 
         public static double FromUnitSpeed(this double speed, string speedUnit) {
@@ -60,7 +72,7 @@ namespace NTMiner {
             if (speed == 0 && newSpeed != 0) {
                 return true;
             }
-            return Math.Abs(speed - newSpeed) / speed >= minChangeScopPercent
-;        }
+            return Math.Abs(speed - newSpeed) / speed >= minChangeScopPercent;
+        }
     }
 }
