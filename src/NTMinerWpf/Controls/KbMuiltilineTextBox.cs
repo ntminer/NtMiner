0836a4142813f0abf77ba0dc5159d80dc1c0ae3a
kuibug <kuibug@outlook.com>
2019-05-30 11:25:32 +0800

    test

---
+++ src/NTMinerWpf/Controls/KbMuiltilineTextBox.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace NTMiner.Controls {
+    public class KbMuiltilineTextBox : TextBox {
+        public KbMuiltilineTextBox() {
+            this.AcceptsReturn = true;
+            this.TextWrapping = System.Windows.TextWrapping.Wrap;
+        }
+    }
+}
