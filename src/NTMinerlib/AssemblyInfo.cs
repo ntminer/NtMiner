7794bf07a72b497cd53f8854d6f171fcd76dc841
kuibug <kuibug@outlook.com>
2019-06-08 00:28:39 +0800

    功耗补偿

--- src/NTMinerlib/AssemblyInfo.cs
+++ src/NTMinerlib/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 namespace NTMiner {
     public static class AssemblyInfo {
         public const string Version = "2.2.0";
-        public const string Build = "2";
+        public const string Build = "3";
         public const string Copyright = "Copyright ©  2019";
         public const string Tag = "蛮吉";
         public static readonly string ServerJsonFileName = $"server2.0.0.json";
