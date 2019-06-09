862d0293be338b3187a29b99a0424f7223266b67
kuibug <kuibug@outlook.com>
2019-06-09 18:07:40 +0800

    test

---
+++ src/NTMiner.Core/Core/Impl/MinerEventSet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NTMiner.MinerClient;
+
+namespace NTMiner.Core.Impl {
+    public class MinerEventSet : IMinerEventSet {
+        public IEnumerable<IMinerEvent> Query(Guid? typeId, string keyword, DateTime? leftTime, DateTime? rightTime) {
+            throw new NotImplementedException();
+        }
+    }
+}
