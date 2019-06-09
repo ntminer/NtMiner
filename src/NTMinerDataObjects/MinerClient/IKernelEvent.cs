e62a320bc9ae6421bacdc8c0f973d04bf53e8624
kuibug <kuibug@outlook.com>
2019-06-09 23:31:25 +0800

    tset

---
+++ src/NTMinerDataObjects/MinerClient/IKernelEvent.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NTMiner.MinerClient {
+    public interface IKernelEvent : ILevelEntity<Guid> {
+        Guid WorkerEventTypeId { get; }
+        string Keyword { get; }
+    }
+}
