787a4be75f673f3034e27f130ff0ab768f537364
kuibug <kuibug@outlook.com>
2019-07-03 01:29:14 +0800

    test

--- src/NTMinerLogging/AssemblyInfo.cs
+++ src/NTMinerLogging/AssemblyInfo.cs
@@ -6,7 +6,7 @@ namespace NTMiner {
     public static class AssemblyInfo {
         public static string GlobalDirFullName { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "NTMiner");
         public const string Version = "2.2.2";
-        public const string Build = "0";
+        public const string Build = "1";
         public const string Copyright = "Copyright ©  2019";
         public const string Tag = "蛮吉";
         public static readonly string ServerJsonFileName = $"server2.0.0.json";
