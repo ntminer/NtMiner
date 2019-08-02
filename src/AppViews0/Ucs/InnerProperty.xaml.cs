c2e7ea025046b1060de69b90570a21988b7a2c47
ntminer <ntminer@none.com>
2019-08-03 00:02:32 +0800

    test

--- src/AppViews0/Ucs/InnerProperty.xaml.cs
+++ src/AppViews0/Ucs/InnerProperty.xaml.cs
@@ -12,5 +12,9 @@ namespace NTMiner.Views.Ucs {
         public InnerProperty() {
             InitializeComponent();
         }
+
+        private void ScrollViewer_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            Wpf.Util.ScrollViewer_PreviewMouseDown(sender, e);
+        }
     }
 }
