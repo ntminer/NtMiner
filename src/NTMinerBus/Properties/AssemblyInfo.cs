08c57c5d4d9761f51377037bc7a66154d6c845c1
ntminer <ntminer@none.com>
2019-08-04 01:01:25 +0800

    test

--- src/NTMinerBus/Properties/AssemblyInfo.cs
+++ src/NTMinerBus/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("NTMiner.Bus")]
+[assembly: AssemblyTitle("NTMinerBus")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NTMiner.Bus")]
+[assembly: AssemblyProduct("NTMinerBus")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
