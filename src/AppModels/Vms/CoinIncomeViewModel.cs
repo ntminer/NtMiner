8da832ce5e246dd6266b9200fb0e1a5abe562ee7
ntminer <ntminer@none.com>
2019-08-06 10:08:46 +0800

    test

--- src/AppModels/Vms/CoinIncomeViewModel.cs
+++ src/AppModels/Vms/CoinIncomeViewModel.cs
@@ -4,9 +4,11 @@ using System.Windows.Media;
 
 namespace NTMiner.Vms {
     public class CoinIncomeViewModel : ViewModelBase {
+        private string _incomePerDaySumText;
+        private string _incomeCnyPerDaySumText;
+        private string _coinPriceCnyText;
         private string _incomePerDayText;
         private string _incomeCnyPerDayText;
-        private string _coinPriceCnyText;
         private readonly CoinViewModel _coinVm;
         private SpeedUnitViewModel _speedUnitVm = null;
         private SolidColorBrush _backgroundBrush;
@@ -23,12 +25,20 @@ namespace NTMiner.Vms {
                 var incomePerDay = NTMinerRoot.Instance.CalcConfigSet.GetIncomePerHashPerDay(_coinVm.Code);
                 var v = this.Speed.FromUnitSpeed(this.SpeedUnitVm.Unit) * incomePerDay.IncomeCoin;
                 if (v >= 100) {
+                    IncomePerDaySumText = v.ToString("f2");
+                }
+                else {
+                    IncomePerDaySumText = v.ToString("f7");
+                }
+                v = 1.0.FromUnitSpeed(this.SpeedUnitVm.Unit) * incomePerDay.IncomeCoin;
+                if (v >= 100) {
                     IncomePerDayText = v.ToString("f2");
                 }
                 else {
                     IncomePerDayText = v.ToString("f7");
                 }
-                IncomeCnyPerDayText = (this.Speed.FromUnitSpeed(this.SpeedUnitVm.Unit) * incomePerDay.IncomeCny).ToString("f2");
+                IncomeCnyPerDaySumText = (this.Speed.FromUnitSpeed(this.SpeedUnitVm.Unit) * incomePerDay.IncomeCny).ToString("f2");
+                IncomeCnyPerDayText = (1.0.FromUnitSpeed(this.SpeedUnitVm.Unit) * incomePerDay.IncomeCny).ToString("f2");
                 CoinPriceCnyText = (incomePerDay.IncomeCny / incomePerDay.IncomeCoin).ToString("f2");
                 ModifiedOn = incomePerDay.ModifiedOn;
                 if (ModifiedOn.AddMinutes(15) < DateTime.Now) {
@@ -40,6 +50,8 @@ namespace NTMiner.Vms {
                 OnPropertyChanged(nameof(SpeedUnitVm));
             }
             else {
+                IncomePerDaySumText = "0";
+                IncomeCnyPerDaySumText = "0";
                 IncomePerDayText = "0";
                 IncomeCnyPerDayText = "0";
                 CoinPriceCnyText = "0";
@@ -99,6 +111,26 @@ namespace NTMiner.Vms {
             set {
                 _incomeCnyPerDayText = value;
                 OnPropertyChanged(nameof(IncomeCnyPerDayText));
+            }
+        }
+
+        public string IncomePerDaySumText {
+            get {
+                return _incomePerDaySumText;
+            }
+            set {
+                _incomePerDaySumText = value;
+                OnPropertyChanged(nameof(IncomePerDaySumText));
+            }
+        }
+
+        public string IncomeCnyPerDaySumText {
+            get {
+                return _incomeCnyPerDaySumText;
+            }
+            set {
+                _incomeCnyPerDaySumText = value;
+                OnPropertyChanged(nameof(IncomeCnyPerDaySumText));
             }
         }
 
--- src/AppViews0/Ucs/Calc.xaml.cs
+++ src/AppViews0/Ucs/Calc.xaml.cs
@@ -15,8 +15,8 @@ namespace NTMiner.Views.Ucs {
             ContainerWindow.ShowWindow(new ContainerWindowViewModel {
                 Title = "收益计算器",
                 IconName = "Icon_Calc",
-                Width = 700,
-                MinWidth = 700,
+                Width = 800,
+                MinWidth = 800,
                 Height = 560,
                 CloseVisible = Visibility.Visible,
                 FooterText = "数据来自鱼池首页，感谢鱼池的支持。因为数据来自矿池，单位算力收益的币数是非常准确的。"
