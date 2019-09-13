2e7e52acb62a3e02ea30cae7ffdbe085ade711a4
nttest <nttest@nttest.test>
2019-09-13 21:23:10 +0800

    test

--- src/ConsoleTests/Program.cs
+++ src/ConsoleTests/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Management;
+using System.Text;
 
 namespace NTMiner {
     class Program {
@@ -6,7 +8,17 @@ namespace NTMiner {
             DevMode.SetDevMode();
 
             ObjectJsonSerializer objectJsonSerializer = new ObjectJsonSerializer();
-
+            ManagementObjectSearcher managementObject = new ManagementObjectSearcher("select * from Win32_VideoController");
+            var str = new StringBuilder();
+            foreach (ManagementObject m in managementObject.Get()) {
+                foreach (var kv in m.Properties) {
+                    str.Append(kv.Name);
+                    str.Append(": ");
+                    str.Append(kv.Value);
+                    str.Append("\n");
+                }
+            }
+            Console.WriteLine(str);
             Console.ReadKey();
         }
     }
