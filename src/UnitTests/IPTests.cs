8fe8685f8a746cd72ee31927adaf2ecb01e2ef6f
ntminer <ntminer@none.com>
2019-10-07 02:54:42 +0800

    test

--- src/UnitTests/IPTests.cs
+++ src/UnitTests/IPTests.cs
@@ -1,20 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NTMiner;
-using NTMiner.Ip.Impl;
 using NTMiner.MinerClient;
 using System;
-using System.Net;
 
 namespace UnitTests {
     [TestClass]
     public class IPTests {
-        [TestMethod]
-        public void GetAllDevicesOnLANTest() {
-            foreach (IPAddress ip in IpSet.Instance) {
-                Console.WriteLine("IP : {0}", ip);
-            }
-        }
-
         [TestMethod]
         public void GetLocalIpTest() {
             foreach (ILocalIp item in VirtualRoot.LocalIpSet) {
