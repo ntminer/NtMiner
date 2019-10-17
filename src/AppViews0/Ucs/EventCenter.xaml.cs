6a769f8357d599dc606ec326e67e946fe47db98c
ntminer <ntminer@none.com>
2019-10-17 16:37:24 +0800

    test

---
+++ src/AppViews0/Ucs/EventCenter.xaml.cs
@@ -0,0 +1,24 @@
+﻿using NTMiner.Vms;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NTMiner.Views.Ucs {
+    public partial class EventCenter : UserControl {
+        private EventCenterViewModel Vm {
+            get {
+                return (EventCenterViewModel)this.DataContext;
+            }
+        }
+
+        public EventCenter() {
+            InitializeComponent();
+        }
+
+        private void ItemsControl_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                Window.GetWindow(this).DragMove();
+            }
+        }
+    }
+}
