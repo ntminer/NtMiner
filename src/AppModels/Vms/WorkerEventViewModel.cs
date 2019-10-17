fa7eea09f1d3076624f89e84e02437d3735f6ae9
ntminer <ntminer@none.com>
2019-10-17 18:32:15 +0800

    test

---
+++ src/AppModels/Vms/WorkerEventViewModel.cs
@@ -0,0 +1,40 @@
+﻿using NTMiner.MinerClient;
+using System;
+
+namespace NTMiner.Vms {
+    public class WorkerEventViewModel : ViewModelBase, IWorkerEvent {
+        private readonly WorkerEventData _data;
+
+        public WorkerEventViewModel(WorkerEventData data) {
+            _data = data;
+        }
+
+        public Guid GetId() {
+            return this.Id;
+        }
+
+        public Guid Id {
+            get {
+                return _data.Id;
+            }
+        }
+
+        public Guid EventTypeId {
+            get {
+                return _data.EventTypeId;
+            }
+        }
+
+        public string Content {
+            get {
+                return _data.Content;
+            }
+        }
+
+        public DateTime EventOn {
+            get {
+                return _data.EventOn;
+            }
+        }
+    }
+}
