f5096aea5db56fe104b874de323dd6bea5d8e3e3
ntminer <ntminer@none.com>
2019-10-18 10:50:56 +0800

    test

--- src/NTMinerDataObjects/MinerClient/IWorkerEvent.cs
+++ src/NTMinerDataObjects/MinerClient/IWorkerEvent.cs
@@ -4,6 +4,9 @@ namespace NTMiner.MinerClient {
     public interface IWorkerEvent : IEntity<int> {
         // Id will be auto-incremented by litedb
         int Id { get; }
+        /// <summary>
+        /// 频道是平的，主题是分层的。开源矿工的挖矿事件没有主题需求。
+        /// </summary>
         WorkerEventChannel Channel { get; }
         string Content { get; }
         DateTime EventOn { get; }
