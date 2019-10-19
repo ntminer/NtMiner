1fb89480a7594edf0e07a85bc8fa1a083631e3ee
ntminer <ntminer@none.com>
2019-10-20 05:44:13 +0800

    test

---
+++ src/NTMinerWpf/Views/Ucs/Icons.xaml.cs
@@ -0,0 +1,16 @@
+﻿using NTMiner.Vms;
+using System.Windows.Controls;
+
+namespace NTMiner.Views.Ucs {
+    public partial class Icons : UserControl {
+        public IconsViewModel Vm {
+            get {
+                return (IconsViewModel)this.DataContext;
+            }
+        }
+
+        public Icons() {
+            InitializeComponent();
+        }
+    }
+}
