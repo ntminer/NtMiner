14dd12dd8fd1b1e82cf679492995e78942d8d8bc
ntminer <ntminer@none.com>
2019-10-20 11:05:37 +0800

    test

--- src/UnitTests/Properties/AssemblyInfo.cs
+++ src/UnitTests/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("4a3c8c8b-6642-405f-b349-57e23903f15b")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
