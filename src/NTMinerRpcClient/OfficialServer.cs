fee29ed78a7adde223523fc9ccaefa9ce87c5064
ntminer <ntminer@none.com>
2019-10-20 11:56:24 +0800

    test

--- src/NTMinerRpcClient/OfficialServer.cs
+++ src/NTMinerRpcClient/OfficialServer.cs
@@ -16,10 +16,14 @@ namespace NTMiner {
         public static readonly CalcConfigServiceFace CalcConfigService = CalcConfigServiceFace.Instance;
 
         public static string SignatureSafeUrl(Uri uri) {
+            // https://ntminer.oss-cn-beijing.aliyuncs.com/packages/HSPMinerAE2.1.2.zip?Expires=1554472712&OSSAccessKeyId=LTAIHNApO2ImeMxI&Signature=FVTf+nX4grLKcPRxpJd9nf3Py7I=
+            // Signature的值长度是28
             string url = uri.ToString();
-            int index = url.IndexOf('?');
+            const string keyword = "Signature=";
+            int index = url.IndexOf(keyword);
             if (index != -1) {
-                return url.Substring(0, index + 1) + HttpUtility.UrlEncode(url.Substring(index + 1));
+                string signature = url.Substring(index + keyword.Length, 28);
+                return url.Substring(0, index) + keyword + HttpUtility.UrlEncode(signature) + url.Substring(index + keyword.Length + 28);
             }
             return url;
         }
