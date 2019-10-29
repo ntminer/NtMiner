4f9c52abccc0a0da4d2f864f903752e58d2f07b3
ntminer <ntminer@none.com>
2019-10-29 17:02:39 +0800

    test

--- src/AppViews0/KernelsWindow.xaml.cs
+++ src/AppViews0/KernelsWindow.xaml.cs
@@ -67,5 +67,52 @@ namespace NTMiner.Views {
                 this.DragMove();
             }
         }
+
+        private void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            WpfUtil.DataGrid_MouseDoubleClick<KernelViewModel>(sender, e);
+        }
+
+        private void ButtonLeftCoin_Click(object sender, RoutedEventArgs e) {
+            double offset = CoinsScrollView.ContentHorizontalOffset - CoinsScrollView.ViewportWidth;
+            CoinsScrollView.ScrollToHorizontalOffset(offset);
+            ButtonLeftCoin.IsEnabled = offset > 0;
+            ButtonRightCoin.IsEnabled = offset < CoinsScrollView.ScrollableWidth;
+        }
+
+        private void ButtonRightCoin_Click(object sender, RoutedEventArgs e) {
+            double offset = CoinsScrollView.ContentHorizontalOffset + CoinsScrollView.ViewportWidth;
+            CoinsScrollView.ScrollToHorizontalOffset(offset);
+            ButtonLeftCoin.IsEnabled = offset > 0;
+            ButtonRightCoin.IsEnabled = offset < CoinsScrollView.ScrollableWidth;
+        }
+
+        private void CoinsScrollView_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            WpfUtil.ScrollViewer_PreviewMouseDown(sender, e);
+        }
+
+        private void ListBox_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                Window window = Window.GetWindow(this);
+                window.DragMove();
+            }
+        }
+
+        private void ButtonLeftBrand_Click(object sender, RoutedEventArgs e) {
+            double offset = BrandsScrollView.ContentHorizontalOffset - BrandsScrollView.ViewportWidth;
+            BrandsScrollView.ScrollToHorizontalOffset(offset);
+            ButtonLeftBrand.IsEnabled = offset > 0;
+            ButtonRightBrand.IsEnabled = offset < BrandsScrollView.ScrollableWidth;
+        }
+
+        private void ButtonRightBrand_Click(object sender, RoutedEventArgs e) {
+            double offset = BrandsScrollView.ContentHorizontalOffset + BrandsScrollView.ViewportWidth;
+            BrandsScrollView.ScrollToHorizontalOffset(offset);
+            ButtonLeftBrand.IsEnabled = offset > 0;
+            ButtonRightBrand.IsEnabled = offset < BrandsScrollView.ScrollableWidth;
+        }
+
+        private void BrandsScrollView_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            WpfUtil.ScrollViewer_PreviewMouseDown(sender, e);
+        }
     }
 }
