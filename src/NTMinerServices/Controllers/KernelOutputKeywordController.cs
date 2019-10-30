685d5b31b237c5c1fe4fe1a25d6c5aa5ad5f380d
ntminer <ntminer@none.com>
2019-10-30 21:44:50 +0800

    test

---
+++ src/NTMinerServices/Controllers/KernelOutputKeywordController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NTMiner.Core;
+using NTMiner.MinerServer;
+
+namespace NTMiner.Controllers {
+    public class KernelOutputKeywordController : ApiControllerBase, IKernelOutputKeywordController {
+        public string GetVersion() {
+            throw new NotImplementedException();
+        }
+
+        public DataResponse<List<KernelOutputKeywordData>> KernelOutputKeywords(KernelOutputKeywordsRequest request) {
+            throw new NotImplementedException();
+        }
+
+        public ResponseBase SetKernelOutputKeyword(DataRequest<KernelOutputKeywordData> request) {
+            throw new NotImplementedException();
+        }
+    }
+}
