8a8150bae2dd45184f1d6a0de2e69d3f25613142
ntminer <ntminer@none.com>
2019-10-30 18:41:22 +0800

    test

---
+++ src/NTMiner.Controllers/IKernelOutputKeywordController.cs
@@ -0,0 +1,11 @@
+﻿using NTMiner.Core;
+using NTMiner.MinerServer;
+using System.Collections.Generic;
+
+namespace NTMiner.Controllers {
+    public interface IKernelOutputKeywordController {
+        string GetVersion();
+        DataResponse<List<KernelOutputKeywordData>> KernelOutputKeywords(KernelOutputKeywordsRequest request);
+        ResponseBase SetKernelOutputKeyword(DataRequest<KernelOutputKeywordData> request);
+    }
+}
