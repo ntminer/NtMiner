02704569ae59be3156b3b00eeb9cf5d5bca2903d
ntminer <ntminer@none.com>
2019-10-31 13:39:37 +0800

    test

--- src/MinerClient/RemoteDesktopEnabler/Firewall.cs
+++ src/MinerClient/RemoteDesktopEnabler/Firewall.cs
@@ -3,18 +3,18 @@ using System;
 using System.Linq;
 
 namespace NTMiner.RemoteDesktopEnabler {
-    internal enum FirewallDomain {
+    public enum FirewallDomain {
         Domain = 0x0001,
         Private = 0x0002,
         Public = 0x0004,
         All = 0x7FFFFFFF
     }
-    internal enum FirewallStatus {
+    public enum FirewallStatus {
         Enabled = 1,
         Disabled = 0
     }
 
-    internal static class Firewall {
+    public static class Firewall {
         private const int RdpTcpPort = 3389;
         private const int RdpUdpPort = 3389;
         private const int RdpScope = 1;
@@ -35,21 +35,8 @@ namespace NTMiner.RemoteDesktopEnabler {
             return (FirewallStatus)Convert.ToInt32(policyManager.get_FirewallEnabled(fwCurrentProfileTypes));
         }
 
-        internal static FirewallStatus Status(FirewallDomain? domain = null) {
+        public static FirewallStatus Status(FirewallDomain? domain = null) {
             return FirewallStatus(domain);
-        }
-
-        private static void OpenPort(string name, int port, NET_FW_IP_PROTOCOL_ protocol, NET_FW_SCOPE_ scope) {
-            INetFwOpenPorts openPorts = GetOpenPorts();
-            if (openPorts.OfType<INetFwOpenPort>().Where(x => x.Name == name).Count() == 0) {
-                INetFwOpenPort openPort = (INetFwOpenPort)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwOpenPort"));
-                openPort.Port = port;
-                openPort.Protocol = protocol;
-                openPort.Scope = scope;
-                openPort.Name = name;
-
-                openPorts.Add(openPort);
-            }
         }
 
         public static void AddRemoteDesktopRule() {
@@ -66,9 +53,23 @@ namespace NTMiner.RemoteDesktopEnabler {
             policyManager.Rules.Remove(FirewallRuleName);
         }
 
-        public static bool RemoteDesktopRuleExists() {
+        public static bool IsRemoteDesktopRuleExists() {
             INetFwPolicy2 policyManager = GetPolicyManager();
             return policyManager.Rules.OfType<INetFwRule>().Where(x => x.Name.StartsWith(FirewallRuleName)).Count() > 0;
+        }
+
+        #region private methods
+        private static void OpenPort(string name, int port, NET_FW_IP_PROTOCOL_ protocol, NET_FW_SCOPE_ scope) {
+            INetFwOpenPorts openPorts = GetOpenPorts();
+            if (openPorts.OfType<INetFwOpenPort>().Where(x => x.Name == name).Count() == 0) {
+                INetFwOpenPort openPort = (INetFwOpenPort)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwOpenPort"));
+                openPort.Port = port;
+                openPort.Protocol = protocol;
+                openPort.Scope = scope;
+                openPort.Name = name;
+
+                openPorts.Add(openPort);
+            }
         }
 
         private static INetFwPolicy2 GetPolicyManager() {
@@ -82,5 +83,6 @@ namespace NTMiner.RemoteDesktopEnabler {
             INetFwOpenPorts openPorts = profile.GloballyOpenPorts;
             return openPorts;
         }
+        #endregion
     }
 }
