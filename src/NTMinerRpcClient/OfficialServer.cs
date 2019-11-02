eb759f431cf5fba278175f24834280734c359dad
ntminer <ntminer@none.com>
2019-11-02 14:02:16 +0800

    test

--- src/NTMinerRpcClient/OfficialServer.cs
+++ src/NTMinerRpcClient/OfficialServer.cs
@@ -15,7 +15,7 @@ namespace NTMiner {
         public static readonly OverClockDataServiceFace OverClockDataService = OverClockDataServiceFace.Instance;
         public static readonly NTMinerWalletServiceFace NTMinerWalletService = NTMinerWalletServiceFace.Instance;
         public static readonly KernelOutputKeywordServiceFace KernelOutputKeywordService = KernelOutputKeywordServiceFace.Instance;
-        public static readonly CalcConfigServiceFace CalcConfigService = CalcConfigServiceFace.Instance;
+        public static readonly ControlCenterServiceFace ControlCenterService = ControlCenterServiceFace.Instance;
 
         public static string SignatureSafeUrl(Uri uri) {
             // https://ntminer.oss-cn-beijing.aliyuncs.com/packages/HSPMinerAE2.1.2.zip?Expires=1554472712&OSSAccessKeyId=LTAIHNApO2ImeMxI&Signature=FVTf+nX4grLKcPRxpJd9nf3Py7I=
