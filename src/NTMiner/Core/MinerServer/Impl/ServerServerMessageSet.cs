450c011f5449a3b5a176a72317232b4036b4fef2
ntminer <ntminer@none.com>
2019-11-03 23:39:53 +0800

    test

---
+++ src/NTMiner/Core/MinerServer/Impl/ServerServerMessageSet.cs
@@ -0,0 +1,33 @@
+﻿using NTMiner.MinerServer;
+using NTMiner.ServerMessage;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NTMiner.Core.MinerServer.Impl {
+    public class ServerServerMessageSet : IServerMessageSet {
+        public int Count => throw new NotImplementedException();
+
+        public void Add(string provider, string messageType, string content) {
+            throw new NotImplementedException();
+        }
+
+        public List<IServerMessage> GetServerMessages(DateTime timeStamp) {
+            throw new NotImplementedException();
+        }
+
+        public void Clear() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<IServerMessage> GetEnumerator() {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+    }
+}
