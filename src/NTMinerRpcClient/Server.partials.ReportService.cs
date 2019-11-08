c1ddce61de31f5ded49952d275a42102452589a2
ntminer <ntminer@none.com>
2019-11-08 13:51:03 +0800

    test

--- src/NTMinerRpcClient/Server.partials.ReportService.cs
+++ src/NTMinerRpcClient/Server.partials.ReportService.cs
@@ -21,12 +21,8 @@ namespace NTMiner {
                             // 可能超过3秒钟，查查原因。因为我的网络不稳经常断线。
                             client.Timeout = timeSpan;
                             Task<HttpResponseMessage> getHttpResponse = client.PostAsJsonAsync($"http://{host}:{NTKeyword.ControlCenterPort}/api/{SControllerName}/{nameof(IReportController.ReportSpeed)}", data);
-                            if (callback != null) {
-                                callback.Invoke(getHttpResponse.Result.Content.ReadAsAsync<ReportResponse>().Result);
-                            }
-                            else {
-                                Write.DevDebug($"{nameof(ReportSpeedAsync)} {getHttpResponse.Result.ReasonPhrase}");
-                            }
+                            ReportResponse response = getHttpResponse.Result.Content.ReadAsAsync<ReportResponse>().Result;
+                            callback?.Invoke(response);
                         }
                     }
                     catch (Exception e) {
