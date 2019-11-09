f16bf5e0f056953f1aae50a7d4c7e265973674f8
ntminer <ntminer@none.com>
2019-11-09 11:25:35 +0800

    test

--- src/NTMinerRpcClient/OfficialServer.partials.ServerMessageServiceFace.cs
+++ src/NTMinerRpcClient/OfficialServer.partials.ServerMessageServiceFace.cs
@@ -13,7 +13,14 @@ namespace NTMiner {
             }
 
             #region GetServerMessagesAsync
+            private DateTime _lastGetServerMessageOn;
             public void GetServerMessagesAsync(DateTime timestamp, Action<DataResponse<List<ServerMessageData>>, Exception> callback) {
+                // 一种防御，简化消费者逻辑
+                if (_lastGetServerMessageOn.AddSeconds(1) >= DateTime.Now) {
+                    callback?.Invoke(new DataResponse<List<ServerMessageData>>(), null);
+                    return;
+                }
+                _lastGetServerMessageOn = DateTime.Now;
                 ServerMessagesRequest request = new ServerMessagesRequest {
                     Timestamp = Timestamp.GetTimestamp(timestamp)
                 };
