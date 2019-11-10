ee26e3cfed8cc76b066873be5c21838203530d7f
ntminer <ntminer@none.com>
2019-11-10 13:56:36 +0800

    test

---
+++ src/UnitTests/StopwatchTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace UnitTests {
+    // 基于栈的Stopwatch
+    [TestClass]
+    public class StopwatchTests {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
