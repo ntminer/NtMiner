96574354a539c1ba549dd9206d4f63a0784e77d3
ntminer <ntminer@none.com>
2019-11-12 14:13:40 +0800

    test

--- src/AppModels/Vms/ServerMessageViewModel.cs
+++ src/AppModels/Vms/ServerMessageViewModel.cs
@@ -82,7 +82,9 @@ namespace NTMiner.Vms {
                     return;
                 }
                 this.ShowDialog(new DialogWindowViewModel(message: $"您确定标记删除'{this.Content}'这条消息吗？", title: "确认", onYes: () => {
-                    VirtualRoot.Execute(new MarkDeleteServerMessageCommand(this.Id));
+                    LoginWindow.Login(() => {
+                        VirtualRoot.Execute(new MarkDeleteServerMessageCommand(this.Id));
+                    });
                 }));
             });
             this.Save = new DelegateCommand(() => {
