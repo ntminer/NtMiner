6e2db198bb9efdc28953c2a3f2a22c23137b1792
ntminer <ntminer@none.com>
2019-11-15 17:23:14 +0800

    test

--- src/AppViews0/MainWindow.xaml.cs
+++ src/AppViews0/MainWindow.xaml.cs
@@ -137,19 +137,13 @@ namespace NTMiner.Views {
             }
 
             UIThread.StartTimer();
-            bool isSplashed = false;
-            ConsoleWindow.Instance.OnSplashHided = ()=> {
-                if (!isSplashed) {
-                    isSplashed = true;
-                    ConsoleWindow.Instance.MouseDown += (sender, e) => {
-                        MoveConsoleWindow();
-                    };
-                }
-                MoveConsoleWindow();
-            };
+            ConsoleWindow.Instance.OnSplashHided = MoveConsoleWindow;
             _borderBrush = this.BorderBrush;
             DateTime lastGetServerMessageOn = DateTime.MinValue;
             NTMinerRoot.RefreshArgsAssembly.Invoke();
+            this.Activated += (sender, e) => {
+                MoveConsoleWindow();
+            };
             // 切换了主界面上的Tab时
             this.MainArea.SelectionChanged += (sender, e) => {
                 var selectedItem = MainArea.SelectedItem;
