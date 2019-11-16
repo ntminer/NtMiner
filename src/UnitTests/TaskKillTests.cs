1b3c1d084dfad2995d6adf457d037ed165541672
ntminer <ntminer@none.com>
2019-11-16 14:35:38 +0800

    test

---
+++ src/UnitTests/TaskKillTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace UnitTests {
+    [TestClass]
+    public class TaskKillTests {
+        [TestMethod]
+        public void KillByPIdTest() {
+            var process = Process.Start(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NTMinerSplash.exe"));
+            NTMiner.Windows.TaskKill.Kill(process.Id);
+        }
+    }
+}
