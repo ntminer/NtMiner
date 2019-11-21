973ffa81d17f2df760bf4a4a8cbee0e15d37db01
ntminer <ntminer@none.com>
2019-11-21 21:53:47 +0800

    test

--- src/ConsoleTests/Program.cs
+++ src/ConsoleTests/Program.cs
@@ -8,43 +8,52 @@ namespace NTMiner {
         static void Main(string[] args) {
             DevMode.SetDevMode();
 
-            //CpuTest();
             WebSocketTest();
 
             Console.ReadKey();
         }
         
-        static void CpuTest() {
-            foreach (var cpu in HardwareProviders.CPU.Cpu.Discover()) {
-                Console.WriteLine(cpu.PackageTemperature?.ToString());
-                foreach (var item in cpu.CoreTemperatures) {
-                    Console.WriteLine(item.Value?.ToString());
-                }
-            }
-        }
-
         static void WebSocketTest() {
-            var allSockets = new List<IWebSocketConnection>();
-            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            Dictionary<Guid, IWebSocketConnection> connDic = new Dictionary<Guid, IWebSocketConnection>();
+            var server = new WebSocketServer($"ws://0.0.0.0:8088");
             server.Start(socket => {
                 socket.OnOpen = () => {
-                    Console.WriteLine("Open!");
-                    allSockets.Add(socket);
+                    string id = socket.ConnectionInfo.Id.ToString();
+                    connDic.Add(socket.ConnectionInfo.Id, socket);
+                    socket.Send($"clientId:" + id);
                 };
                 socket.OnClose = () => {
-                    Console.WriteLine("Close!");
-                    allSockets.Remove(socket);
+                    connDic.Remove(socket.ConnectionInfo.Id);
                 };
                 socket.OnMessage = message => {
-                    Console.WriteLine(message);
-                    allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+                    if (string.IsNullOrEmpty(message)) {
+                        return;
+                    }
+                    string[] parts = message.Split(':');
+                    if (parts.Length < 2) {
+                        return;
+                    }
+                    if (!Guid.TryParse(parts[0], out Guid clientId) || !connDic.ContainsKey(clientId)) {
+                        return;
+                    }
+                    string command = parts[1];
+                    switch (command) {
+                        case "getSpeed":
+                            if (connDic.TryGetValue(clientId, out IWebSocketConnection conn)) {
+                                conn.Send("result of getSpeed:{'a':'this is a test'}");
+                            }
+                            break;
+                        default:
+                            connDic.Values.ToList().ForEach(s => s.Send("Echo:" + command));
+                            break;
+                    }
                 };
             });
 
 
             var input = Console.ReadLine();
             while (input != "exit") {
-                foreach (var socket in allSockets.ToList()) {
+                foreach (var socket in connDic.Values.ToList()) {
                     socket.Send(input);
                 }
                 input = Console.ReadLine();
