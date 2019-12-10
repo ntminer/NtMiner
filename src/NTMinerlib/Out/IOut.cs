e7cf7638e0bc642a9ba91f3ac74eb007f7fe00ce
ntminer <ntminer@none.com>
2019-12-10 12:10:20 +0800

    test

--- src/NTMinerlib/Out/IOut.cs
+++ src/NTMinerlib/Out/IOut.cs
@@ -1,4 +1,4 @@
-﻿namespace NTMiner {
+﻿namespace NTMiner.Out {
     public interface IOut {
         /// <param name="autoHideSeconds">0表示不自动隐藏，即需要手动点击"知道了"按钮隐藏</param>
         void ShowError(string message, int autoHideSeconds = 0);
--- src/NTMinerlib/VirtualRoot.cs
+++ src/NTMinerlib/VirtualRoot.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Text;
+using NTMiner.Out;
 
 namespace NTMiner {
     /// <summary>
@@ -129,30 +130,6 @@ namespace NTMiner {
                 return _out ?? EmptyOut.Instance;
             }
         }
-
-        #region 这是一个外部不需要知道的类型
-        private class EmptyOut : IOut {
-            public static readonly EmptyOut Instance = new EmptyOut();
-
-            private EmptyOut() { }
-
-            public void ShowError(string message, int autoHideSeconds) {
-                // nothing need todo
-            }
-
-            public void ShowInfo(string message, int autoHideSeconds) {
-                // nothing need todo
-            }
-
-            public void ShowSuccess(string message, int autoHideSeconds, string header = "成功") {
-                // nothing need todo
-            }
-
-            public void ShowWarn(string message, int autoHideSeconds) {
-                // nothing need todo
-            }
-        }
-        #endregion
 
         public static void SetOut(IOut ntOut) {
             _out = ntOut;
