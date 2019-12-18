78e8b5cbda2dd62ae6a6282fcd1710c0c71110d3
ntminer <ntminer@none.com>
2019-12-18 13:22:16 +0800

    test

--- src/NTMinerlib/DelayTask.cs
+++ src/NTMinerlib/DelayTask.cs
@@ -6,7 +6,9 @@ namespace NTMiner {
     public static class DelayTask {
         public static Task Delay(this TimeSpan timeSpan) {
             var tcs = new TaskCompletionSource<object>();
+#pragma warning disable IDE0067 // 丢失范围之前释放对象
             var timer = new Timer(timeSpan.TotalMilliseconds) { AutoReset = false };
+#pragma warning restore IDE0067 // 丢失范围之前释放对象
             timer.Elapsed += (sender, e) => {
                 timer.Dispose();
                 tcs.SetResult(null);
@@ -17,7 +19,9 @@ namespace NTMiner {
 
         public static Task MillisecondsDelay(this int n) {
             var tcs = new TaskCompletionSource<object>();
+#pragma warning disable IDE0067 // 丢失范围之前释放对象
             var timer = new Timer(n) { AutoReset = false };
+#pragma warning restore IDE0067 // 丢失范围之前释放对象
             timer.Elapsed += (sender, e)=> {
                 timer.Dispose();
                 tcs.SetResult(null);
