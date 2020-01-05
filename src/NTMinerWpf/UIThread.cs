3446f6a5c7587f75a006dccc3ddf21965bf9447a
ntminer <ntminer@none.com>
2020-01-05 13:31:59 +0800

    test

--- src/NTMinerWpf/UIThread.cs
+++ src/NTMinerWpf/UIThread.cs
@@ -17,7 +17,8 @@ namespace NTMiner {
         /// <summary>
         /// 因为该方法可能会在非UI线程被调用，所以是这个风格。
         /// 详解：当以UIThread.Execute(Vm.Method1)这个风格调用时，因为Vm实例可能来自
-        /// 于(TVm)this.DataContext，而this是DependencyObject是不能在非UI线程访问的。
+        /// 于(TVm)this.DataContext，而this.DataContext是依赖属性，依赖属性在Wpf中是
+        /// 通过GetValue()静态方法访问的，而GetValue()方法中会VerifyAccess()。
         /// </summary>
         public static void Execute(Func<Action> getAction) {
             if (_dispatcher.CheckAccess()) {
---
+++ src/UnitTests/DependencyObjectTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UnitTests {
+    [TestClass]
+    public class DependencyObjectTests {
+        [TestMethod]
+        public void DependencyObjectTest() {
+            DependencyObject obj = null;
+            Task.Factory.StartNew(() => {
+                obj = new DependencyObject();
+            }).Wait();
+            Assert.IsNotNull(obj);
+            Assert.IsFalse(obj.Dispatcher.CheckAccess());
+        }
+
+        [TestMethod]
+        public void DependencyObjectTest1() {
+            DependencyObject obj = new DependencyObject();
+            Assert.IsTrue(obj.Dispatcher.CheckAccess());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void DependencyObjectTest2() {
+            DependencyObject obj = null;
+            Task.Factory.StartNew(() => {
+                obj = new DependencyObject();
+            }).Wait();
+            obj.GetValue(FrameworkElement.WidthProperty);
+        }
+    }
+}
