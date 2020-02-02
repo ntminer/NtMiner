e06ae9931cccc5afb63e441e85c407a36142e04e
ntminer <ntminer@none.com>
2020-02-02 18:09:33 +0800

    test

---
+++ src/AppViews0/Ucs/AppMiner.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace NTMiner.Views.Ucs {
+    public partial class AppMiner : UserControl {
+        public AppMiner() {
+            InitializeComponent();
+        }
+    }
+}
