7a8a03d84974b41cc2adaa96ce6463fa609d315c
ntminer <ntminer@none.com>
2020-02-04 12:48:04 +0800

    test

--- src/WsCommands/WsCommandBase.cs
+++ src/WsCommands/WsCommandBase.cs
@@ -4,12 +4,16 @@ using WebSocketSharp.Server;
 
 namespace WsCommands {
     public abstract class WsCommandBase : ICmd {
+        private readonly Guid _messageId = Guid.NewGuid();
+
         public WsCommandBase(string sessionId, WebSocketSessionManager sessions) {
             this.SessionId = sessionId;
             this.Sessions = sessions;
         }
 
-        public Guid MessageId { get; private set; } = Guid.NewGuid();
+        public Guid MessageId {
+            get { return _messageId; }
+        }
         public string SessionId { get; private set; }
         public WebSocketSessionManager Sessions { get; private set; }
     }
