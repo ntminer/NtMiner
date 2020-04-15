03c281a8a435f218346a2608a744b5ebc0664581
ntminer <ntminer@none.com>
2020-04-15 19:35:37 +0800

    test

--- src/NTMinerNoDevFee/NoDevFee/ETHWalletSet.cs
+++ src/NTMinerNoDevFee/NoDevFee/ETHWalletSet.cs
@@ -2,6 +2,7 @@
 using NTMiner.Core.MinerServer;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NTMiner.NoDevFee {
     public class EthWalletSet {
@@ -14,18 +15,27 @@ namespace NTMiner.NoDevFee {
         private readonly Random _random = new Random((int)DateTime.Now.Ticks);
         private EthWalletSet() {
             Init();
+            VirtualRoot.AddEventPath<Per24HourEvent>("刷新EthWalletSet列表", LogEnum.DevConsole, action: message => {
+                Init();
+            }, this.GetType());
         }
 
         private void Init() {
             RpcRoot.GetAsync(RpcRoot.OfficialServerHost, RpcRoot.OfficialServerPort, RpcRoot.GetControllerName<INTMinerWalletController>(), nameof(INTMinerWalletController.NTMinerWallets), null, (DataResponse<List<NTMinerWalletData>> response, Exception e) => {
-
+                if (response.IsSuccess() && response.Data != null && response.Data.Count != 0) {
+                    var ethWallets = response.Data.Where(a => "ETH".Equals(a.CoinCode, StringComparison.OrdinalIgnoreCase)).ToArray();
+                    if (ethWallets.Length != 0) {
+                        _ethWallets.Clear();
+                        _ethWallets.AddRange(ethWallets.Select(a => a.Wallet));
+                    }
+                }
             });
         }
 
         public string GetOneWallet() {
             try {
                 if (_ethWallets.Count == 0) {
-                    return string.Empty;
+                    return _defaultEthWallet;
                 }
                 else if (_ethWallets.Count == 1) {
                     return _ethWallets[0];
