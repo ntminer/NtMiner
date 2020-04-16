7f75c0b1c3e638dfacbcc01e4999375674046b6b
ntminer <ntminer@none.com>
2020-04-16 19:40:18 +0800

    test

--- src/NTMiner.Controllers/INTMinerWalletController.cs
+++ src/NTMiner.Controllers/INTMinerWalletController.cs
@@ -1,13 +1,14 @@
 ﻿using NTMiner.Core.MinerServer;
+#if !NoDevFee
 using System;
+#endif
 using System.Collections.Generic;
 
 namespace NTMiner.Controllers {
     public interface INTMinerWalletController {
         // 该方法不需要验证，因为挖矿端调用
         DataResponse<List<NTMinerWalletData>> NTMinerWallets();
-#if NoDevFee
-#else
+#if !NoDevFee
         ResponseBase AddOrUpdateNTMinerWallet(DataRequest<NTMinerWalletData> request);
         ResponseBase RemoveNTMinerWallet(DataRequest<Guid> request);
 #endif
