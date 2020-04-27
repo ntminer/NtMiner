642edfb571136988d94a390680754569a8d2630a
ntminer <ntminer@none.com>
2020-04-28 02:56:24 +0800

    test

--- src/AppViews0/MinerStudio/Views/Ucs/MineWorkEdit.xaml.cs
+++ src/AppViews0/MinerStudio/Views/Ucs/MineWorkEdit.xaml.cs
@@ -6,8 +6,9 @@ using System.Windows.Controls;
 namespace NTMiner.MinerStudio.Views.Ucs {
     public partial class MineWorkEdit : UserControl {
         public static void ShowWindow(FormType formType, MineWorkViewModel source) {
+            string title = source.IsSelfMineWork ? "自主作业" : "挖矿作业 — 作业通常用于让不同的矿机执行同样的挖矿任务";
             ContainerWindow.ShowWindow(new ContainerWindowViewModel {
-                Title = "挖矿作业 — 作业通常用于让不同的矿机执行同样的挖矿任务",
+                Title = title,
                 FormType = formType,
                 IsMaskTheParent = true,
                 Width = 900,
