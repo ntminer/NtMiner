5aa59a5915099029a4c74acefb699dbcd0a89447
ntminer <ntminer@none.com>
2020-04-28 14:26:21 +0800

    test

---
+++ src/NTMinerClient/WorkType.cs
@@ -0,0 +1,16 @@
+﻿namespace NTMiner {
+    public enum WorkType {
+        /// <summary>
+        /// 未使用作业
+        /// </summary>
+        None,
+        /// <summary>
+        /// 单机作业
+        /// </summary>
+        SelfWork,
+        /// <summary>
+        /// 统一作业
+        /// </summary>
+        MineWork
+    }
+}
