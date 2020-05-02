5e5aef9196c9e8d482d9b43827cbd5f7678c238d
ntminer <ntminer@none.com>
2020-05-02 18:02:52 +0800

    test

--- src/NTMinerDataSchemas/User/WsUserName.cs
+++ src/NTMinerDataSchemas/User/WsUserName.cs
@@ -11,6 +11,10 @@ namespace NTMiner.User {
         public string ClientVersion { get; set; }
         public Guid ClientId { get; set; }
         public string UserId { get; set; }
+        /// <summary>
+        /// 表示是否支持收发二进制格式的WsMessage。
+        /// </summary>
+        public bool IsBinarySupported { get; set; }
 
         public bool IsValid() {
             return !string.IsNullOrEmpty(UserId) && ClientId != Guid.Empty && Version.TryParse(ClientVersion, out _);
