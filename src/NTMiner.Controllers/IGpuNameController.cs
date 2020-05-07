b874888b9d4b11e8b8536348a0c365aa381c9d4a
ntminer <ntminer@none.com>
2020-05-07 16:43:03 +0800

    test

---
+++ src/NTMiner.Controllers/IGpuNameController.cs
@@ -0,0 +1,16 @@
+﻿using NTMiner.Core.Gpus;
+using System.Collections.Generic;
+
+namespace NTMiner.Controllers {
+    public interface IGpuNameController {
+        DataResponse<List<GpuName>> GpuNames(object request);
+        /// <summary>
+        /// 需签名
+        /// </summary>
+        ResponseBase SetGpuName(DataRequest<GpuName> request);
+        /// <summary>
+        /// 需签名
+        /// </summary>
+        ResponseBase RemoveGpuName(DataRequest<GpuName> request);
+    }
+}
---
+++ src/WebApiServer/Controllers/GpuNameController.cs
@@ -0,0 +1,35 @@
+﻿using NTMiner.Core.Gpus;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace NTMiner.Controllers {
+    public class GpuNameController : IGpuNameController {
+        [HttpGet]
+        [HttpPost]
+        public DataResponse<List<GpuName>> GpuNames(object request) {
+            if (request == null) {
+                return ResponseBase.InvalidInput<DataResponse<List<GpuName>>>("参数错误");
+            }
+            return DataResponse<List<GpuName>>.Ok(WebApiRoot.GpuNameSet.AsEnumerable().ToList());
+        }
+
+        [HttpPost]
+        public ResponseBase SetGpuName(DataRequest<GpuName> request) {
+            if (request == null || request.Data == null) {
+                return ResponseBase.InvalidInput("参数错误");
+            }
+            WebApiRoot.GpuNameSet.Set(request.Data);
+            return ResponseBase.Ok("设置成功");
+        }
+
+        [HttpPost]
+        public ResponseBase RemoveGpuName(DataRequest<GpuName> request) {
+            if (request == null || request.Data == null) {
+                return ResponseBase.InvalidInput("参数错误");
+            }
+            WebApiRoot.GpuNameSet.Remove(request.Data);
+            return ResponseBase.Ok("移除成功");
+        }
+    }
+}
