013eeb4d1d2a325eeb30af426b01c06cb6eafa27
ntminer <ntminer@none.com>
2020-05-15 17:47:48 +0800

    test

--- src/NTMinerHub/Messages.cs
+++ src/NTMinerHub/Messages.cs
@@ -24,162 +24,188 @@ namespace NTMiner {
     [MessageType(description: "已经启动1秒钟", isCanNoPath: true)]
     public class HasBoot1SecondEvent : EventBase {
         public readonly int Seconds = 1;
+
+        internal HasBoot1SecondEvent() { }
     }
 
     [MessageType(description: "已经启动2秒钟", isCanNoPath: true)]
     public class HasBoot2SecondEvent : EventBase {
         public readonly int Seconds = 2;
+
+        internal HasBoot2SecondEvent() { }
     }
 
     [MessageType(description: "已经启动5秒钟", isCanNoPath: true)]
     public class HasBoot5SecondEvent : EventBase {
         public readonly int Seconds = 5;
+
+        internal HasBoot5SecondEvent() { }
     }
 
     [MessageType(description: "已经启动10秒钟", isCanNoPath: true)]
     public class HasBoot10SecondEvent : EventBase {
         public readonly int Seconds = 10;
+
+        internal HasBoot10SecondEvent() { }
     }
 
     [MessageType(description: "已经启动20秒钟", isCanNoPath: true)]
     public class HasBoot20SecondEvent : EventBase {
         public readonly int Seconds = 20;
+
+        internal HasBoot20SecondEvent() { }
     }
 
     [MessageType(description: "已经启动1分钟", isCanNoPath: true)]
     public class HasBoot1MinuteEvent : EventBase {
         public readonly int Seconds = 60;
+
+        internal HasBoot1MinuteEvent() { }
     }
 
     [MessageType(description: "已经启动2分钟", isCanNoPath: true)]
     public class HasBoot2MinuteEvent : EventBase {
         public readonly int Seconds = 120;
+
+        internal HasBoot2MinuteEvent() { }
     }
 
     [MessageType(description: "已经启动5分钟", isCanNoPath: true)]
     public class HasBoot5MinuteEvent : EventBase {
         public readonly int Seconds = 300;
+
+        internal HasBoot5MinuteEvent() { }
     }
 
     [MessageType(description: "已经启动10分钟", isCanNoPath: true)]
     public class HasBoot10MinuteEvent : EventBase {
         public readonly int Seconds = 600;
+
+        internal HasBoot10MinuteEvent() { }
     }
 
     [MessageType(description: "已经启动20分钟", isCanNoPath: true)]
     public class HasBoot20MinuteEvent : EventBase {
         public readonly int Seconds = 1200;
+
+        internal HasBoot20MinuteEvent() { }
     }
 
     [MessageType(description: "已经启动50分钟", isCanNoPath: true)]
     public class HasBoot50MinuteEvent : EventBase {
         public readonly int Seconds = 3000;
+
+        internal HasBoot50MinuteEvent() { }
     }
 
     [MessageType(description: "已经启动100分钟", isCanNoPath: true)]
     public class HasBoot100MinuteEvent : EventBase {
         public readonly int Seconds = 6000;
+
+        internal HasBoot100MinuteEvent() { }
     }
 
     [MessageType(description: "已经启动24小时", isCanNoPath: true)]
     public class HasBoot24HourEvent : EventBase {
         public readonly int Seconds = 86400;
+
+        internal HasBoot24HourEvent() { }
     }
 
     [MessageType(description: "每1秒事件", isCanNoPath: true)]
     public class Per1SecondEvent : EventBase {
         public readonly int Seconds = 1;
 
-        public Per1SecondEvent() { }
+        internal Per1SecondEvent() { }
     }
 
     [MessageType(description: "每2秒事件", isCanNoPath: true)]
     public class Per2SecondEvent : EventBase {
         public readonly int Seconds = 2;
 
-        public Per2SecondEvent() { }
+        internal Per2SecondEvent() { }
     }
 
     [MessageType(description: "每5秒事件", isCanNoPath: true)]
     public class Per5SecondEvent : EventBase {
         public readonly int Seconds = 5;
 
-        public Per5SecondEvent() { }
+        internal Per5SecondEvent() { }
     }
 
     [MessageType(description: "每10秒事件", isCanNoPath: true)]
     public class Per10SecondEvent : EventBase {
         public readonly int Seconds = 10;
 
-        public Per10SecondEvent() { }
+        internal Per10SecondEvent() { }
     }
 
     [MessageType(description: "每20秒事件", isCanNoPath: true)]
     public class Per20SecondEvent : EventBase {
         public readonly int Seconds = 20;
 
-        public Per20SecondEvent() { }
+        internal Per20SecondEvent() { }
     }
 
     [MessageType(description: "时间的分钟部分变更后", isCanNoPath: true)]
     public class MinutePartChangedEvent : EventBase {
-        public MinutePartChangedEvent() { }
+        internal MinutePartChangedEvent() { }
     }
 
     [MessageType(description: "每1分钟事件", isCanNoPath: true)]
     public class Per1MinuteEvent : EventBase {
         public readonly int Seconds = 60;
 
-        public Per1MinuteEvent() { }
+        internal Per1MinuteEvent() { }
     }
 
     [MessageType(description: "每2分钟事件", isCanNoPath: true)]
     public class Per2MinuteEvent : EventBase {
         public readonly int Seconds = 120;
 
-        public Per2MinuteEvent() { }
+        internal Per2MinuteEvent() { }
     }
 
     [MessageType(description: "每5分钟事件", isCanNoPath: true)]
     public class Per5MinuteEvent : EventBase {
         public readonly int Seconds = 300;
 
-        public Per5MinuteEvent() { }
+        internal Per5MinuteEvent() { }
     }
 
     [MessageType(description: "每10分钟事件", isCanNoPath: true)]
     public class Per10MinuteEvent : EventBase {
         public readonly int Seconds = 600;
 
-        public Per10MinuteEvent() { }
+        internal Per10MinuteEvent() { }
     }
 
     [MessageType(description: "每20分钟事件", isCanNoPath: true)]
     public class Per20MinuteEvent : EventBase {
         public readonly int Seconds = 1200;
-        public Per20MinuteEvent() { }
+        internal Per20MinuteEvent() { }
     }
 
     [MessageType(description: "每50分钟事件", isCanNoPath: true)]
     public class Per50MinuteEvent : EventBase {
         public readonly int Seconds = 3000;
-        public Per50MinuteEvent() { }
+        internal Per50MinuteEvent() { }
     }
 
     [MessageType(description: "每100分钟事件", isCanNoPath: true)]
     public class Per100MinuteEvent : EventBase {
         public readonly int Seconds = 6000;
-        public Per100MinuteEvent() { }
+        internal Per100MinuteEvent() { }
     }
 
     [MessageType(description: "每24小时事件", isCanNoPath: true)]
     public class Per24HourEvent : EventBase {
         public readonly int Seconds = 86400;
-        public Per24HourEvent() { }
+        internal Per24HourEvent() { }
     }
 
     [MessageType(description: "新的一天到来了（刚过24点，0点0分1秒的时候）", isCanNoPath: true)]
     public class NewDayEvent : EventBase {
-        public NewDayEvent() { }
+        internal NewDayEvent() { }
     }
 }
