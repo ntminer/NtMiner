b7270e584b4f00b6814989309c94ae8109ec68c7
ntminer <ntminer@none.com>
2020-05-24 14:21:28 +0800

    test

--- src/AppViews0/Views/ConsoleWindow.xaml.cs
+++ src/AppViews0/Views/ConsoleWindow.xaml.cs
@@ -16,8 +16,8 @@ namespace NTMiner.Views {
             this.Loaded += (sender, e) => {
                 _thisWindowHandle = new WindowInteropHelper(this).Handle;
                 IntPtr console = NTMinerConsole.GetOrAlloc();
-                SafeNativeMethods.SetParent(console, _thisWindowHandle);
                 SafeNativeMethods.SetWindowLong(console, SafeNativeMethods.GWL_STYLE, SafeNativeMethods.WS_VISIBLE);
+                SafeNativeMethods.SetParent(console, _thisWindowHandle);
                 hwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
                 hwndSource.AddHook(new HwndSourceHook(WndProc));
                 if (AppUtil.IsHotKeyEnabled) {
@@ -97,16 +97,6 @@ namespace NTMiner.Views {
             _marginTop = marginTop;
             _height = height;
             _width = width;
-            // 如果没有ConsoleBgRectangle的话鼠标会点击到桌面上
-            if (ConsoleBgRectangle.Width != width) {
-                ConsoleBgRectangle.Width = width;
-            }
-            if (ConsoleBgRectangle.Height != height) {
-                ConsoleBgRectangle.Height = height;
-            }
-            if ((int)ConsoleBgRectangle.Margin.Top != marginTop) {
-                ConsoleBgRectangle.Margin = new Thickness(0, marginTop, 1, 0);
-            }
             NTMinerConsole.MoveWindow(marginLeft, marginTop, width, height, true);
         }
     }
