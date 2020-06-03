8aa954ac2a16f59fc85e7a4c83583944ef494eeb
ntminer <ntminer@none.com>
2020-06-03 13:24:28 +0800

    test

--- src/WsServer/Messages.cs
+++ src/WsServer/Messages.cs
@@ -3,14 +3,14 @@
 namespace NTMiner {
     [MessageType(description: "打扫时间到，保持清洁")]
     public class CleanTimeArrivedEvent : EventBase {
-        /// <summary>
-        /// seconds传0表示不需要清理death session
-        /// </summary>
-        /// <param name="nodeAddresses"></param>
+        /// <param name="nodeAddresses">在线的节点</param>
         public CleanTimeArrivedEvent(string[] nodeAddresses) {
             this.NodeAddresses = nodeAddresses;
         }
 
+        /// <summary>
+        /// 在线的节点
+        /// </summary>
         public string[] NodeAddresses { get; private set; }
     }
 
