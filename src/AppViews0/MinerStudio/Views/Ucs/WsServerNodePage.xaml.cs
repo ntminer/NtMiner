930663a82b0cbd31b0cc28ac9d36b2b3c5ce0a87
ntminer <test@test.test>
2021-03-30 10:37:13 +0800

    test

--- src/AppViews0/MinerStudio/Views/Ucs/WsServerNodePage.xaml.cs
+++ src/AppViews0/MinerStudio/Views/Ucs/WsServerNodePage.xaml.cs
@@ -11,7 +11,7 @@ namespace NTMiner.MinerStudio.Views.Ucs {
                 Title = "外网群控websocket服务器节点",
                 IconName = "Icon_Server",
                 Width = 1590,
-                Height = 500,
+                Height = 400,
                 IsMaskTheParent = false,
                 IsChildWindow = true,
                 CloseVisible = Visibility.Visible,
