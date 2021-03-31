680baec192d305cd641a7251c962b4bee5f5a064
ntminer <test@test.test>
2021-03-31 16:38:20 +0800

    test

---
+++ src/NTMinerDataSchemas/Core/UserGetSpeedData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NTMiner.Core {
+    public class UserGetSpeedData {
+        public UserGetSpeedData() {
+            this.ClientIds = new List<Guid>();
+        }
+
+        public string LoginName { get; set; }
+        public List<Guid> ClientIds { get; set; }
+    }
+}
