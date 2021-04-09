8ba2673296d16cb6b3099f85e1fecc4eb5d6fcab
ThunderEX <thunderex@gmail.com>
2021-04-09 10:44:15 +0800

    add self into windows defender exclusion path, instead of disable windows defender

--- src/NTMinerClient/Windows/Defender.cs
+++ src/NTMinerClient/Windows/Defender.cs
@@ -1,20 +1,54 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
 
 namespace NTMiner.Windows {
     public static class Defender {
-        public static void DisableAntiSpyware() {
+        public static void DisableAntiSpyware(string homePath, string tempPath) {
             try {
-                const string subKey = @"SOFTWARE\Policies\Microsoft\Windows Defender";
-                var v = WinRegistry.GetValue(Registry.LocalMachine, subKey, "DisableAntiSpyware");
-                if (v == null || v.ToString() != "1") {
-                    WinRegistry.SetValue(Registry.LocalMachine, subKey, "DisableAntiSpyware", 1);
-                    Logger.OkDebugLine("Windows Defender禁用成功");
+                if (!TryGetIsPathExcluded(homePath))
+                {
+                    AddAntiVirusExclusion(homePath);
                 }
+                if (!TryGetIsPathExcluded(tempPath))
+                {
+                    AddAntiVirusExclusion(tempPath);
+                }
+                Logger.OkDebugLine("Windows Defender禁用成功");
             }
             catch (Exception e) {
                 Logger.ErrorDebugLine("Windows Defender禁用失败，因为异常", e);
             }
+        }
+
+        public static void AddAntiVirusExclusion(string path)
+        {
+            using (PowerShell PowerShellInst = PowerShell.Create())
+            {
+                PowerShellInst.AddScript(@"Add-MpPreference -ExclusionPath '" + path + "'");
+                PowerShellInst.Invoke();
+            }
+        }
+
+        public static bool TryGetIsPathExcluded(string path)
+        {
+            using (PowerShell PowerShellInst = PowerShell.Create())
+            {
+                PowerShellInst.AddScript(@"(Get-MpPreference).ExclusionPath");
+                Collection<PSObject> PSOutput = PowerShellInst.Invoke();
+                foreach (PSObject outputItem in PSOutput)
+                {
+                    if (outputItem != null)
+                    {
+                        if (outputItem.BaseObject.ToString().Trim().Equals(path, StringComparison.OrdinalIgnoreCase))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
         }
     }
 }
